e4d14906974176c8e1c3412fb77342bd4042e40f
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-11-20 09:57:23 +0330

    Added media settings

--- WSS_Site/settings.py
+++ WSS_Site/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
