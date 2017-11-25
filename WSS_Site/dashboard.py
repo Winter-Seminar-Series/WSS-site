012fbfba5377234f74d4a808f91e5e376b104532
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-11-25 20:29:44 +0330

    removed google analytics

--- WSS_Site/dashboard.py
+++ WSS_Site/dashboard.py
@@ -1,17 +1,13 @@
-from django.utils.translation import ugettext_lazy as _
 from jet.dashboard import modules
 from jet.dashboard.dashboard import Dashboard
-from jet.dashboard.dashboard_modules import google_analytics
 
 
 class IndexDashboard(Dashboard):
-    columns = 3
+    columns = 2
 
     def init_with_context(self, context):
-        self.available_children.append(google_analytics.GoogleAnalyticsVisitorsTotals)
-        self.available_children.append(google_analytics.GoogleAnalyticsVisitorsChart)
-        self.available_children.append(google_analytics.GoogleAnalyticsPeriodVisitors)
         self.available_children.append(modules.LinkList)
+
         self.children.append(modules.LinkList(
             'About Creators of this Website',
             children=[
