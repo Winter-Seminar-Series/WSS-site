5268462fe9742e11ef33451d7597e6b3edb55283
somagh <aryakowsary@yahoo.com>
2017-11-30 19:06:58 +0330

    bug fixed and workshop detailview added

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from events.views import SeminarView
+from events.views import SeminarView, WorkshopView
 
 urlpatterns = [
     url(r'seminar/(?P<pk>\d+)/$', SeminarView.as_view(), name='seminar'),
+    url(r'workshop/(?P<pk>\d+)/$', WorkshopView.as_view(), name='workshop'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,11 +1,25 @@
-# Create your views here.
+ # Create your views here.
 from django.views.generic import DetailView
 
 from WSS.mixins import ExternalLinkMixin
-from events.models import Seminar
+from WSS.models import WSS
+from events.models import Seminar, Workshop
 
 
 class SeminarView(ExternalLinkMixin,DetailView):
     template_name = 'events/seminar.html'
     context_object_name = 'seminar'
     model = Seminar
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data()
+        context['wss']=self.object.wss
+        return context
+
+class WorkshopView(ExternalLinkMixin,DetailView):
+    template_name = 'events/workshop.html'
+    context_object_name = 'workshop'
+    model = Workshop
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data()
+        context['wss']=self.object.wss
+        return context
