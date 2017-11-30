aab9c8b27ad07055f51553a14dac98c9bfb39e93
somagh <aryakowsary@yahoo.com>
2017-11-30 18:11:24 +0330

    externalLink new

--- WSS/mixins.py
+++ WSS/mixins.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 
-from WSS.models import WSS
+from WSS.models import WSS,ExternalLink
 
 
 class WSSWithYearMixin:
@@ -9,3 +9,8 @@ class WSSWithYearMixin:
     def get_object(self, queryset=None):
         return get_object_or_404(WSS, year=int(self.kwargs['year']))
 
+class ExternalLinkMixin:
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data()
+        context['external_links'] = ExternalLink.objects.all()
+        return context
--- events/views.py
+++ events/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
 from django.views.generic import DetailView
 
+from WSS.mixins import ExternalLinkMixin
 from events.models import Seminar
 
 
-class SeminarView(DetailView):
+class SeminarView(ExternalLinkMixin,DetailView):
     template_name = 'events/seminar.html'
     context_object_name = 'seminar'
     model = Seminar
