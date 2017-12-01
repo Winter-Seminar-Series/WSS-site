58cdef5eb65dfd561d3f0e0721031b195dc92e2c
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-12-01 12:01:56 +0330

    added schedule to navbar

--- WSS_Site/urls.py
+++ WSS_Site/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from WSS_Site.handler_views import Handler404View
+from WSS_Site.error_handler_views import Handler404View
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),
