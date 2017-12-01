f80dd7cbec32ec5a82c0cd11f521e6704f4f9f99
somagh <aryakowsary@yahoo.com>
2017-12-01 17:52:41 +0330

    better template inheritance

--- templatetags/no_wss_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.loader import render_to_string
-
-register = template.Library()
-
-
-@register.simple_tag
-def no_wss_navbar():
-    return render_to_string('no_wss_navbar.html')
