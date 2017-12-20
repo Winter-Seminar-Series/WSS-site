fa78c4e3a467c5c28c12ac0b7a0e00def8bde3bd
kianoosh.abbasi76 <kianoosh.abbasi76@gmail.com>
2017-12-20 12:34:29 +0330

    code reformat

--- WSS/mixins.py
+++ WSS/mixins.py
@@ -13,6 +13,7 @@ class WSSWithYearMixin:
 class FooterMixin:
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['past_years'] = [q[0] for q in WSS.objects.exclude(pk=WSS.active_wss().pk).values_list('year')]
+        context['past_years'] = [q[0] for q in
+                                 WSS.objects.exclude(pk=WSS.active_wss().pk).values_list('year')]
         context['external_links'] = ExternalLink.objects.all()
         return context
