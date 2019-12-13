f6014d80e541bc35d5581d646984ebf5308648ba
Kiarash Golzadeh <kiarash.gl@gmail.com>
2019-12-14 00:25:40 +0330

    Database

--- WSS_Site/settings.py
+++ WSS_Site/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'WSS_Site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'org2',
-        'USER': 'sarh',
-        'PASSWORD': '147852',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
