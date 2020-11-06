6c1b6adca2b6e242a54b289b32d8d690e1cc0107
ahmadsalimi <ahsa9978@gmail.com>
2020-11-06 21:06:29 +0330

    add footermixin

--- api/views.py
+++ api/views.py
@@ -8,21 +8,7 @@ from api.serializer import WorkshopSerializer
 from events.models import Workshop
 from WSS.models import WSS
 
-# from .models import Task
-# Create your views here.
-
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>/',
-        'Create': '/task-create/',
-        'Update': '/task-update/<str:pk>/',
-        'Delete': '/task-delete/<str:pk>/',
-    }
-    return Response(api_urls)
-
-class WorkshopViewSet(viewsets.ViewSet):
+class WorkshopViewSet(viewsets.ViewSet, FooterMixin):
     """
     A simple ViewSet for listing or retrieving users.
     """
@@ -37,10 +23,3 @@ class WorkshopViewSet(viewsets.ViewSet):
         serializer = WorkshopSerializer(user)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def list_workshops(request):
-    print(request.__dict__)
-    workshops = WSS.objects.first().workshops
-    WSS.objects.filter()
-    serializer = WorkshopSerializer(workshops, many=True)
-    return Response(serializer.data)
