ffbb2bfc6c38ab39bd47bf254a61cb656209d991
ahmadsalimi <ahsa9978@gmail.com>
2020-11-06 20:58:26 +0330

    add viewset

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
-from . import views
+from api.views import WorkshopViewSet
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
-    path('workshops', views.list_workshops, name="list-workshops")
-    # path('task-list', views.taskList, name="task-list"),
-    # path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
-    # path('task-create/', views.taskCreate, name="task-create"),
 
-    # path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
-    # path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
-]
+router = DefaultRouter()
+router.register(r'workshops', WorkshopViewSet, basename='workshop')
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+#     path('', views.apiOverview, name="api-overview"),
+#     path('workshops', views.list_workshops, name="list-workshops")
+#     # path('task-list', views.taskList, name="task-list"),
+#     # path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+#     # path('task-create/', views.taskCreate, name="task-create"),
+
+#     # path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
+#     # path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
+# ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
 from rest_framework.response import Response
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
@@ -21,9 +22,25 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
+class WorkshopViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = Workshop.objects.all()
+        serializer = WorkshopSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Workshop.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = WorkshopSerializer(user)
+        return Response(serializer.data)
+
 @api_view(['GET'])
 def list_workshops(request):
     print(request.__dict__)
     workshops = WSS.objects.first().workshops
+    WSS.objects.filter()
     serializer = WorkshopSerializer(workshops, many=True)
     return Response(serializer.data)
