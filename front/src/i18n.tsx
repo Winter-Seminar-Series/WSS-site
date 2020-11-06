69df99b67d56e78eee199a91e9b106f4d3f4bad3
fatteme <fatt3me@gmail.com>
2020-11-06 23:20:18 +0330

    remove unnecessary packages

--- front/src/i18n.tsx
+++ front/src/i18n.tsx
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
 
-void i18n
+i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import './i18n';
 
 ReactDOM.render(
