5bf17e756fe6f3ac1ad31ead6f53ccdcf228695d
fatteme <fatt3me@gmail.com>
2020-11-06 22:54:32 +0330

    improvements in header url and hrefs

--- front/src/views/Home/header.tsx
+++ front/src/views/Home/header.tsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <nav className="navbar navbar-expand-xl navbar-dark w-100">
       <div className="container">
-        <a className="navbar-brand" href="{% url 'wss:home' %}">
-          <img src="{% static 'images/logo.png' %}" width="50px" alt="" />
+        <a className="navbar-brand" href="/home">
+          <img src="images/logo.png" width="50px" alt="" />
         </a>
         <button
           className="navbar-toggler"
@@ -20,12 +20,12 @@ function Header() {
         <div className="collapse navbar-collapse" id="wss-navbar">
           <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="{% url 'wss:home' %}">
+              <a className="nav-link" href="/home">
                 Home
               </a>
             </li>
             <li className="header-ticket nav-item">
-              <a href="{% url 'wss:home' %}" className="btn-white btn-primary">
+              <a href="/home" className="btn-white btn-primary">
                 Visit the last seminar page
               </a>
             </li>
