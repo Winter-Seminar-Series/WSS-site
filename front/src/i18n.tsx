7307145c82da955fde1122cc178d52f045d9d7d6
fatteme <fatt3me@gmail.com>
2020-11-06 19:43:12 +0330

    it runs

--- front/src/i18n.tsx
+++ front/src/i18n.tsx
@@ -1,15 +1,16 @@
-import i18n from "i18next";
-import Backend from "i18next-xhr-backend";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import { initReactI18next } from 'react-i18next';
 
-i18n
+void i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en", debug: false,
+    fallbackLng: 'en',
+    debug: false,
     interpolation: {
-      escapeValue: false
-    }
+      escapeValue: false,
+    },
   });
 
 export default i18n;
