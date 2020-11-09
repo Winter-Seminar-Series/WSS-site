9faf61df97b4971f2801858873b5c27b28ff3250
amooHashem <amoo.hashem.mehraban@gmail.com>
2020-11-09 21:22:56 +0330

    initialize redux

---
+++ front/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import './i18n';
+import configureStore from './redux/store/configureStore';
+
+
+const persistedState = localStorage.getItem('WSS') //use local storage just to save token
+  ? JSON.parse(localStorage.getItem('WSS'))
+  : {};
+
+const store = configureStore(persistedState);
+store.subscribe(() => {
+  const state = store.getState();
+  localStorage.setItem(
+    'WSS',
+    JSON.stringify({
+      account: state.account,
+    })
+  );
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ front/src/redux/actions/urls.js
@@ -0,0 +1,5 @@
+export const ROOT = 'https://???/api/'; // todo
+
+export const REGISTER = ROOT.concat('register/')
+export const LOGIN = ROOT.concat('login/')
+export const LOGOUT = ROOT.concat('logout/')
