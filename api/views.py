cc5bb1e414cce7eea04c691813e8ffe7804cf0ad
ahmadsalimi <ahsa9978@gmail.com>
2020-11-09 10:18:13 +0330

    make it cleaner

--- api/views.py
+++ api/views.py
@@ -13,10 +13,12 @@ def get_wss_object_or_404(year):
 
 class BaseViewSet(viewsets.ViewSet):
 
-    def __init__(self, serializer, queryset_selector, **initkwargs):
-        super().__init__(**initkwargs)
-        self.serializer = serializer
-        self.queryset_selector = queryset_selector
+    @property
+    def serializer(self):
+        raise NotImplementedError()
+    
+    def queryset_selector(self, wss):
+        raise NotImplementedError()
 
     def get_list(self, wss):
         queryset = self.queryset_selector(wss)
@@ -39,6 +41,7 @@ class BaseViewSet(viewsets.ViewSet):
 
 
 class WorkshopViewSet(BaseViewSet):
+    serializer = WorkshopSerializer
 
-    def __init__(self, **initkwargs):
-        super().__init__(WorkshopSerializer, lambda wss: wss.workshops, **initkwargs)
+    def queryset_selector(self, wss):
+        return wss.workshops
