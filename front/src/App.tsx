12dad3479bb7524ad2d6dc647891859e171ee267
fatteme <fatt3me@gmail.com>
2020-11-11 21:33:13 +0330

    make style like django version

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -18,26 +18,21 @@ function App() {
   return (
     <>
       <Header></Header>
-      <div className="body-inner">
-        <header id="header" className="header-dark">
-          <div className="container"></div>
-        </header>
-        <Router>
-          <Switch>
-            <Route path="/details" component={Details} />
-            <Route path="/register" component={Register} />
-            <Route path="/schedule" component={Schedule} />
-            <Route path="/staff-list" component={StaffList} />
-            <Route path="/image-gallery" component={ImageGallery} />
-            <Route path="/video-gallery" component={VideoGallery} />
-            <Route path="/workshop-list" component={WorkshopList} />
-            <Route path="/seminar-list" component={SeminarList} />
-            <Route path="/about" component={About} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </Router>
-        <Footer></Footer>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/details" component={Details} />
+          <Route path="/register" component={Register} />
+          <Route path="/schedule" component={Schedule} />
+          <Route path="/staff-list" component={StaffList} />
+          <Route path="/image-gallery" component={ImageGallery} />
+          <Route path="/video-gallery" component={VideoGallery} />
+          <Route path="/workshop-list" component={WorkshopList} />
+          <Route path="/seminar-list" component={SeminarList} />
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+      <Footer></Footer>
     </>
   );
 }
--- front/src/views/Layout/footer.tsx
+++ front/src/views/Layout/footer.tsx
@@ -2,51 +2,52 @@ import React from 'react';
 
 function Footer() {
   const footerStyle = {
-    marginTop: '4.75rem',
+    marginTop: '2rem',
   };
   return (
-    <footer
-      id="footer"
-      className="text-white footer text-center diagonal blue-gradient pt-5"
-      style={footerStyle}>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="footer-logo">
-              <a href="{{ SITE_URL }}">
-                <img
-                  src="images/WSS-2018-logo-white.png"
-                  alt="footer logo"
-                  width="100"
-                />
-              </a>
-              <a href="http://ssc.ce.sharif.edu">
-                <img
-                  src="images/SSC-logo-white.png"
-                  alt="footer logo"
-                  width="100"
-                />
-              </a>
-            </div>
-            <div className="row">
-              <div className="col-sm-6">
-                <div className="footer-social d-flex justify-content-center justify-content-sm-start">
-                  <ul>
-                    <li>
-                      <a href="{{ external_link.url }}">
-                        <i className="social-icon text-white fa fa-{{ external_link.type }}" />
-                      </a>
-                    </li>
-                  </ul>
+    <footer id="footer" className="text-white footer text-center">
+      <div className="diagonal blue-gradient pt-5" style={footerStyle}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="footer-logo">
+                <a href="{{ SITE_URL }}">
+                  <img
+                    src="images/WSS-2018-logo-white.png"
+                    alt="footer logo"
+                    width="100"
+                  />
+                </a>
+                <a href="http://ssc.ce.sharif.edu">
+                  <img
+                    src="images/SSC-logo-white.png"
+                    alt="footer logo"
+                    width="100"
+                  />
+                </a>
+              </div>
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="footer-social d-flex justify-content-center justify-content-sm-start">
+                    <ul>
+                      <li>
+                        <a href="{{ external_link.url }}">
+                          <i className="social-icon text-white fa fa-{{ external_link.type }}" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mx-auto copyright-info">
-              <span>
-                <a href="{% url 'people:creators-list' %}">Website Creators</a>
-                <br />
-                Copyright © 2015-2019 WSS. All Rights Reserved.
-              </span>
+              <div className="mx-auto copyright-info">
+                <span>
+                  <a href="{% url 'people:creators-list' %}">
+                    Website Creators
+                  </a>
+                  <br />
+                  Copyright © 2015-2019 WSS. All Rights Reserved.
+                </span>
+              </div>
             </div>
           </div>
         </div>
