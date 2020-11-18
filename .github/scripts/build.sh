310a386e4322fda1bf747a210737cace7f710e6f
Emran Batmanghelich <emran.bm@gmail.com>
2020-11-18 21:42:00 +0330

    Refactor smoke test

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,13 +4,10 @@ set -euo pipefail
 
 readonly DOCKER_USERNAME="$1"
 readonly DOCKER_PASSWORD="$2"
-readonly GITHUB_RUN_ID="$3"
-
-echo "DOCKER_USERNAME = $DOCKER_USERNAME"
-echo "GITHUB_RUN_ID = $GITHUB_RUN_ID"
+readonly TAG="$3"
 
 docker pull $DOCKER_USERNAME/web:latest || echo "Unable to pull cache. exit code: $?"
-WEB_IMAGE_VERSION=snapshot-$GITHUB_RUN_ID
+export WEB_IMAGE_VERSION=$TAG
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker-compose build
 docker-compose push web
