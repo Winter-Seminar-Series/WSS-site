2d2da4431e995dd05a7567b4a7cee36c88333524
Emran Batmanghelich <emran.bm@gmail.com>
2020-11-21 00:02:06 +0330

    route front as base.

--- backend/WSS_Site/settings.py
+++ backend/WSS_Site/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend-statics/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
