74d1c4d978b33c08a53f7585c6e07671926188b1
AlirezaT99 <tajmirriahia@yahoo.com>
2020-11-24 19:54:33 +0330

    Serializer for user and registration added

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -3,6 +3,8 @@ from events.models import Workshop, Seminar, PosterSession
 from people.models import HoldingTeam
 from WSS.models import WSS, Sponsorship, Clip, Booklet, Image
 
+from django.contrib.auth.models import User
+
 
 class WSSSerializer(ModelSerializer):
     class Meta:
@@ -70,3 +72,24 @@ class ImageSerializer(ModelSerializer):
     class Meta:
         model = Image
         fields = '__all__'
+
+
+# User Serializer
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+# Register Serializer
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['username'], validated_data['email'], validated_data['password'])
+        # TODO UserProfile object should be created right about here I suppose..
+        return user
