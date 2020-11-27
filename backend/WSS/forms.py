7c33c82d4a73dbaf746ecf89bdde6575a16f84e9
seyedalireza <shosseini@ce.sharif.edu>
2020-11-27 12:36:54 +0330

    Feature: add tags to all events (#43)

--- backend/WSS/forms.py
+++ backend/WSS/forms.py
@@ -41,10 +41,11 @@ class ParticipantForm(forms.ModelForm):
 
     class Meta:
         model = models.UserProfile
-        fields = ['phone_number', 'age', 'job', 'university',
-                  'introduction_method', 'gender', 'city', 'country', 'grade', 'is_student', 'interests']
+        fields = ['phone_number', 'age', 'job', 'university', 'introduction_method',
+                  'gender', 'city', 'country', 'grade', 'is_student', 'interests', 'favorite_tags']
         widgets = {
-            'gender': forms.RadioSelect
+            'gender': forms.RadioSelect,
+            'favorite_tags': forms.CheckboxSelectMultiple
         }
 
     def __init__(self, *args, **kwargs):
