613aca1c315e62d031b5068d6c0c152909d1ef5f
Seyed Alireza <33568929+seyedalireza@users.noreply.github.com>
2020-11-29 00:37:38 +0330

    Rename migration

--- backend/WSS/migrations/0014_userprofile_favorite_tags.py
+++ backend/WSS/migrations/0014_userprofile_favorite_tags.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0005_auto_20201128_2048'),
+        ('events', '0005_event_tag'),
         ('WSS', '0013_making_userprofile_fields_nullable'),
     ]
 
