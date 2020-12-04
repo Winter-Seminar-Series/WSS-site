cea5f0bef1088b32e6dc2ad0ad11fbdaae24d8cb
AlirezaT99 <tajmirriahia@yahoo.com>
2020-12-04 11:02:07 +0330

    Added wss email to host user

--- backend/WSS_Site/settings.py
+++ backend/WSS_Site/settings.py
@@ -250,5 +250,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'TODO@EMRAN.MERCI'
+EMAIL_HOST_USER = 'wss.sharif@gmail.com'
 EMAIL_HOST_PASSWORD = 'WSS_PASSWORD'
