b79c338c72639e5e5e81bd90cac7076d927d5329
mmahdibarghi <mmahdibarghi7980@gmail.com>
2020-12-07 01:22:20 +0330

    package.json updates & urls.tsx changed

--- front/src/redux/actions/urls.tsx
+++ front/src/redux/actions/urls.tsx
@@ -2,7 +2,7 @@ export const ROOT =
   process.env.NODE_ENV === 'production'
     ? 'https://localhost/api/'
     : 'https://cors-anywhere.herokuapp.com/https://sharif-wss.ir/api/';
-
+    
 export const REGISTER = ROOT.concat('register/');
 export const LOGIN = ROOT.concat('login/');
 export const LOGOUT = ROOT.concat('logout/');
