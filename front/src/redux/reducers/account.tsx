7e6b37feb62c701e29a97c5bfdf7aa4ca02c0fe9
AmooHashem <45389673+AmooHashem@users.noreply.github.com>
2020-12-07 07:02:33 +0330

    Seperate front (#107)
    * add some initial code for APIs
    
    * do some cleaning
    
    * login and register api are almost ready
    
    * signup and login page added
    
    * gitignore changed
    
    * add signup apis
    
    * add login api
    
    * made cchanges in header, footer, background images
    
    * add home page attributes
    
    * add modelList api
    
    * add model list count api
    
    * fix a bug
    
    * some cleanup
    
    * add presenter card
    
    * fix a bug
    
    * add toast
    
    * add signup and signin notifications
    
    * add static data to home page
    
    * dashboard added
    
    * did some refactor
    
    * almost complete card
    
    * fix some bug
    
    * change dashboard pages
    
    * fix style problem with react router
    
    * change Home page video
    
    * add auth-guard
    
    * fix home venue background
    
    * fix sign-in style
    
    * signup and login style got back
    
    * seminar-registraion page  added
    
    * remove unnecessary images
    
    * fix some bug + add get_workshops to workshop_registration page
    
    * fix some bug
    
    * fix wrong commit
    
    * add api for workshop registration
    
    * handle redirect when not logged in
    
    * test data + add captcha
    
    * add incomplete payment functions
    
    * add description page
    
    * edit card
    
    * do some cleanup
    
    * remove style.min.css
    
    * fix additional space on top
    
    * fix spacing problem inn a better way
    
    * fix about page
    
    * fix like :(
    
    * home page completed
    
    * speakers page added
    
    * package.json updates & urls.tsx changed
    
    * landing completed
    
    * add get-one-entity functions
    
    * some changes to login and register
    
    * redirect dashboard to seminar registration
    
    * fix homepage cards
    
    * fix homepage cards
    
    * fix a bug
    
    * seminarRegistation added
    
    * fix some paths
    
    * fix some paths
    
    * complete seminar detail page
    
    * fix login and customize header based on loggedIn
    
    * change footer color
    
    * fix not showing logo problem
    
    * add favicon
    
    * add all detailed page
    
    * registration works (it seems)
    
    * fix some paths
    
    * done
    
    Co-authored-by: fateme <fateme@fatemes-MacBook-Pro-3.local>
    Co-authored-by: fatteme <fatt3me@gmail.com>
    Co-authored-by: mmahdibarghi <mmahdibarghi7980@gmail.com>
    Co-authored-by: fatteme khashei <45355613+fatteme@users.noreply.github.com>

---
+++ front/src/redux/reducers/account.tsx
@@ -0,0 +1,108 @@
+import * as actionTypes from '../actionTypes';
+
+const initState = {
+  isFetching: false,
+  isLoggedIn: false,
+  id: '',
+  username: '',
+  email: '',
+  token: '',
+  expiry: '',
+};
+
+function Account(state = initState, action) {
+  switch (action.type) {
+    case actionTypes.REGISTER_REQUEST:
+      return {
+        isFetching: true,
+        isLoggedIn: false,
+      };
+
+    case actionTypes.REGISTER_SUCCESS:
+      return {
+        id: action.response.user.id,
+        username: action.response.user.username,
+        email: action.response.user.email,
+        token: action.response.token,
+        isLoggedIn: true,
+        isFetching: false,
+      };
+
+    case actionTypes.REGISTER_FAILURE:
+      console.log(action.response);
+      return {
+        isFetching: false,
+      };
+
+    ///////////////////
+
+    case actionTypes.LOGIN_REQUEST:
+      return {
+        isLoggedIn: false,
+        isFetching: true,
+      };
+
+    case actionTypes.LOGIN_SUCCESS:
+      return {
+        isFetching: false,
+        isLoggedIn: true,
+        expiry: action.response.expiry,
+        token: action.response.token,
+      };
+
+    case actionTypes.LOGIN_FAILURE:
+      console.log(action.response);
+      return {
+        isFetching: false,
+      };
+
+    ///////////////////
+
+    case actionTypes.LOGOUT_REQUEST:
+      return {
+        isFetching: true,
+      };
+
+    case actionTypes.LOGOUT_SUCCESS:
+      return initState;
+
+    case actionTypes.LOGOUT_FAILURE:
+      return {
+        isFetching: false,
+      };
+
+    ///////////////////
+
+    case actionTypes.SEND_PAYMENT_SUCCESS:
+      var link = document.createElement('a');
+      link.href = action.response.redirect_url;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      return {
+        ...state,
+        redirect_url: action.response.redirect_url,
+      };
+
+    case actionTypes.VERIFY_PAYMENT_SUCCESS:
+      console.log('hey');
+      return {
+        ...state,
+        payment: {
+          // url: action.response.message,
+          // amount: action.response.amount,
+          // typePayment: action.response.typePayment,
+        },
+      };
+    case actionTypes.REMOVE_PAYMENT_DATA:
+      return {
+        ...state,
+        payment: null,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export default Account;
--- front/src/views/Dashboard/Profile.tsx
+++ front/src/views/Dashboard/Profile.tsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getProfile } from '../../redux/actions/participant';
 
-function Profile() {
+function Profile({
+  getProfile,
+  isFetching,
+  isLoggedIn,
+  id,
+  username,
+  email,
+  token,
+  expiry,
+  first_name,
+  last_name,
+  phone_number,
+  age,
+  job,
+  university,
+  introduction_method,
+  gender,
+  city,
+  country,
+  field_of_interest,
+  grade,
+  is_student,
+  favorite_tags,
+}) {
   return <>this is profile</>;
 }
 
-export default Profile;
+const mapStateToProps = (state, ownProps) => {
+  const {
+    isFetching,
+    username,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    age,
+    job,
+    university,
+    introduction_method,
+    gender,
+    city,
+    country,
+    field_of_interest,
+    grade,
+    is_student,
+    favorite_tags,
+  } = state.Participant;
+  return {
+    isFetching,
+    username,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    age,
+    job,
+    university,
+    introduction_method,
+    gender,
+    city,
+    country,
+    field_of_interest,
+    grade,
+    is_student,
+    favorite_tags,
+  };
+};
+
+export default connect(mapStateToProps, {
+  getProfile,
+})(Profile);
