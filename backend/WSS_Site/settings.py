6158056128ae62525ef282638379a6c8d8f46119
Ahmad Salimi <44668836+ahmadsalimi@users.noreply.github.com>
2020-12-08 01:30:09 +0330

    fix migration (#112)
    * fix migration
    
    * fix migrations

--- backend/WSS_Site/settings.py
+++ backend/WSS_Site/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'WSS_Site.urls'
