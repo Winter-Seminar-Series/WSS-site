ae1ffc668cbcb04b836d79e9febb162385023974
Emran BatmanGhelich <emran.bm@gmail.com>
2020-12-07 21:36:07 +0330

    Revert "Test data (#109)"
    This reverts commit 58824f482f872d2fbd840cdaf4ec91459ab17df8.

--- backend/WSS_Site/settings.py
+++ backend/WSS_Site/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.environ.get('WSS_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -65,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'WSS_Site.urls'
