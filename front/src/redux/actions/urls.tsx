24d18d5ca5e8af75a4db881fce9f01acbdb79a80
Emran Batmanghelich <emran.bm@gmail.com>
2020-12-09 23:36:21 +0330

    Build: Auto-switch to sharif-wss.ir BASE_URL in prod.

--- front/src/redux/actions/urls.tsx
+++ front/src/redux/actions/urls.tsx
@@ -1,8 +1,6 @@
-export const ROOT =
-  process.env.NODE_ENV === 'production'
-    ? 'https://localhost/api/'
-    : 'https://cors-anywhere.herokuapp.com/https://sharif-wss.ir/api/';
-    
+import {BASE_URL} from '../../constants/info'
+
+export const ROOT = BASE_URL.concat('/')
 export const REGISTER = ROOT.concat('register/');
 export const LOGIN = ROOT.concat('login/');
 export const LOGOUT = ROOT.concat('logout/');
