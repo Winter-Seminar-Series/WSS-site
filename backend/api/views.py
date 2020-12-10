27716d4fe5b1fa40700076af1e4aacbe35ffd654
Ahmad Salimi <44668836+ahmadsalimi@users.noreply.github.com>
2020-12-10 15:45:59 +0330

    fix payment verify (#138)
    * fix payment verify
    
    * raise exception if authority is None

--- backend/api/views.py
+++ backend/api/views.py
@@ -357,9 +357,16 @@ class PaymentViewSet(viewsets.ViewSet):
                 "message": "You already have finished your payment."
             })        
 
-        if request.GET.get('Status') == 'OK':
+        if request.query_params.get('Status', None) == 'OK':
+            authority = request.query_params.get('Authority', None)
+            if authority is None:
+                return ErrorResponse({
+                    'message': 'Authority should be passed in query string'
+                }, status_code=403)
+            
             amount = wss.registration_fee
-            result = verify(request.GET['Authority'], amount)
+
+            result = verify(authority, amount)
             
             if result.Status == 100:
                 participant = Participant(current_wss=wss, user_profile=user_profile,
