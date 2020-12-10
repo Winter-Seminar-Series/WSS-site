d579673fafeb31f521982cd88dba142f1805246a
AlirezaT99 <tajmirriahia@yahoo.com>
2020-12-10 20:09:04 +0330

    Added passwordReset urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('change-password/', ChangePasswordView.as_view(), name='change-password')
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
