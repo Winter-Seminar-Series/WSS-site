74d80a836b0b2beab41f5b339bd10959963c85e4
Emran Batmanghelich <emran.bm@gmail.com>
2020-12-10 13:27:27 +0330

    Fix: Switch BASE_URL on runtime not build-time

--- front/src/constants/info.ts
+++ front/src/constants/info.ts
@@ -1,2 +1,8 @@
+declare global {
+    interface Window {
+        BASE_URL: string;
+    }
+}
+
 export const THIS_YEAR = 2020;
-export const BASE_URL = 'https://localhost';
+export const BASE_URL = window.BASE_URL;
