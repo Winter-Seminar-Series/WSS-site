29e34106e7d2d48a67dd114eba9e1edc584b3692
AlirezaT99 <tajmirriahia@yahoo.com>
2020-12-10 18:54:57 +0330

    Added ChangePassword view

--- backend/api/views.py
+++ backend/api/views.py
@@ -25,6 +25,9 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from templates.consts import *
 
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated  
+
 
 def get_wss_object_or_404(year: int) -> WSS:
     return get_object_or_404(WSS, year=year)
@@ -424,3 +427,30 @@ class LoginAPI(KnoxLoginView):
         user = serializer.validated_data['user']
         login(request, user)
         return super(LoginAPI, self).post(request, format=None)
+ 
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = serializers.ChangePasswordSerializer
+    model = User
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": "Wrong password"}, status=status.HTTP_400_BAD_REQUEST)
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+
+            return Response({
+                'message': 'Password updated successfully'
+            }, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
