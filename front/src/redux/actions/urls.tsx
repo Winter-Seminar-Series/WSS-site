80dfea7092cbba562bbe503ce913692d695c8a82
AmooHashem <45389673+AmooHashem@users.noreply.github.com>
2020-12-10 05:22:00 +0330

    Update urls.tsx

--- front/src/redux/actions/urls.tsx
+++ front/src/redux/actions/urls.tsx
@@ -1,6 +1,6 @@
 import {BASE_URL} from '../../constants/info'
 
-export const ROOT = BASE_URL.concat('/')
+export const ROOT = BASE_URL.concat('/api/')
 export const REGISTER = ROOT.concat('register/');
 export const LOGIN = ROOT.concat('login/');
 export const LOGOUT = ROOT.concat('logout/');
