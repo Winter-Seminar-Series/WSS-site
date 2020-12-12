d2ec6c2b1454f3bf0024552d2cfa6b1af099f044
LordLoss <a.a.ghanati@gmail.com>
2020-12-12 20:50:07 +0330

    fixed bug in dependency

--- backend/events/migrations/0008_fill_more_seminars.py
+++ backend/events/migrations/0008_fill_more_seminars.py
@@ -81,7 +81,7 @@ def rollback(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('people', '0018_update_holding_team_orders'),
+        ('people', '0017_update_holding_team_orders'),
         ('events', '0007_add_ctype_to_seminar'),
     ]
 
