965076096685253ecdbdbfb8214a59220a86e787
amoohashem <amoo.hashem.mehraban@gmail.com>
2020-12-13 04:16:41 +0330

    fix a bug

--- front/src/views/Dashboard/Profile.tsx
+++ front/src/views/Dashboard/Profile.tsx
@@ -91,207 +91,208 @@ function Profile({
         introduction_method,
       });
     };
-
-    return (
-      <>
-        <div className="diagonal seminar-register-title background-theme d-flex align-items-center">
-          <div className="header ml-3">
-            <div className="title">
-              Profile
-          </div>
-          </div>
-        </div>
-        <form className="seminar-register-form" onSubmit={doUpdateProfile}>
-          <h3>
-            Required fields
-        </h3>
-          <br />
-          <div className="row">
-            <div className="col-12 mb-3 col-lg mb-lg-0">
-              <input
-                value={first_name}
-                onChange={(e) => setFirstName(e.target.value)}
-                type="text"
-                className="text-input form-control"
-                placeholder="First name"
-              />
-            </div>
-            <div className="col-12 col-lg">
-              <input
-                value={last_name}
-                onChange={(e) => setLastName(e.target.value)}
-                type="text"
-                className="text-input form-control"
-                placeholder="Last name"
-              />
-            </div>
-          </div>
-
-
-          <div className="row">
-            <div className="col-12 mb-3 col-lg mb-lg-0">
-              <input
-                value={university}
-                onChange={(e) => setUniversity(e.target.value)}
-                type="text"
-                className="text-input form-control"
-                placeholder="University"
-              />
-            </div>
-            <div className="col-12 col-lg">
-              <input
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-                type="text"
-                className="text-input form-control"
-                placeholder="City"
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-12 mb-3 col-lg mb-lg-0">
-              <input
-                disabled={true}
-                value={email}
-                type="email"
-                className="text-input form-control"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-
-          <div className="row">
-            <div className='col-6 col-md-4'>
-              <div className="form-label pt-0 mr-3">Gender:</div>
-              <div className="dropdown">
-                <button
-                  className="btn dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  {gender}
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  {genderTypes.map((g, index) => (
-                    <a
-                      key={index}
-                      className="dropdown-item"
-                      onClick={() => setGender(g)}>
-                      {g}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className='col-6  col-md-4'>
-              <div className="form-label pt-0 mr-3">Grade:</div>
-              <div className="dropdown">
-                <button
-                  className="btn dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  {grade}
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  {gradeTypes.map((d, index) => (
-                    <a
-                      key={index}
-                      className="dropdown-item"
-                      onClick={() => setGrade(d)}>
-                      {d}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className='col-12 col-md-4'>
-              <div className="form-label pt-0 mr-3">Introduction method:</div>
-              <div className="dropdown">
-                <button
-                  className="btn dropdown-toggle"
-                  type="button"
-                  id="dropdownMenuButton"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  {introduction_method}
-                </button>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  {introductionTypes.map((i, index) => (
-                    <a
-                      key={index}
-                      className="dropdown-item"
-                      onClick={() => setIntroduction_method(i)}>
-                      {i}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <button
-            disabled={isFetching}
-            type="submit"
-            className="btn btn-lg btn-primary btn-dark mb-5"
-          >
-            Update
-        </button>
-        </form>
-      </>
-    );
   }
 
-  const mapStateToProps = (state, ownProps) => {
-    const {
-      isFetching,
-      username,
-      email,
-      first_name,
-      last_name,
-      phone_number,
-      age,
-      job,
-      university,
-      introduction_method,
-      gender,
-      city,
-      country,
-      field_of_interest,
-      grade,
-      is_student,
-      favorite_tags,
-    } = state.Participant;
-    return {
-      isFetching,
-      username,
-      email,
-      first_name,
-      last_name,
-      phone_number,
-      age,
-      job,
-      university,
-      introduction_method,
-      gender,
-      city,
-      country,
-      field_of_interest,
-      grade,
-      is_student,
-      favorite_tags,
-    };
-  };
+  return (
+    <>
+      <div className="diagonal seminar-register-title background-theme d-flex align-items-center">
+        <div className="header ml-3">
+          <div className="title">
+            Profile
+          </div>
+        </div>
+      </div>
+      <form className="seminar-register-form" onSubmit={doUpdateProfile}>
+        <h3>
+          Required fields
+        </h3>
+        <br />
+        <div className="row">
+          <div className="col-12 mb-3 col-lg mb-lg-0">
+            <input
+              value={first_name}
+              onChange={(e) => setFirstName(e.target.value)}
+              type="text"
+              className="text-input form-control"
+              placeholder="First name"
+            />
+          </div>
+          <div className="col-12 col-lg">
+            <input
+              value={last_name}
+              onChange={(e) => setLastName(e.target.value)}
+              type="text"
+              className="text-input form-control"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
 
-  export default connect(
-    mapStateToProps,
-    {
-      getProfile,
-      updateProfile,
-    })(Profile);
+
+        <div className="row">
+          <div className="col-12 mb-3 col-lg mb-lg-0">
+            <input
+              value={university}
+              onChange={(e) => setUniversity(e.target.value)}
+              type="text"
+              className="text-input form-control"
+              placeholder="University"
+            />
+          </div>
+          <div className="col-12 col-lg">
+            <input
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              type="text"
+              className="text-input form-control"
+              placeholder="City"
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12 mb-3 col-lg mb-lg-0">
+            <input
+              disabled={true}
+              value={email}
+              type="email"
+              className="text-input form-control"
+              placeholder="Email"
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className='col-6 col-md-4'>
+            <div className="form-label pt-0 mr-3">Gender:</div>
+            <div className="dropdown">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                {gender}
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                {genderTypes.map((g, index) => (
+                  <a
+                    key={index}
+                    className="dropdown-item"
+                    onClick={() => setGender(g)}>
+                    {g}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='col-6  col-md-4'>
+            <div className="form-label pt-0 mr-3">Grade:</div>
+            <div className="dropdown">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                {grade}
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                {gradeTypes.map((d, index) => (
+                  <a
+                    key={index}
+                    className="dropdown-item"
+                    onClick={() => setGrade(d)}>
+                    {d}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='col-12 col-md-4'>
+            <div className="form-label pt-0 mr-3">Introduction method:</div>
+            <div className="dropdown">
+              <button
+                className="btn dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false">
+                {introduction_method}
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                {introductionTypes.map((i, index) => (
+                  <a
+                    key={index}
+                    className="dropdown-item"
+                    onClick={() => setIntroduction_method(i)}>
+                    {i}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          disabled={isFetching}
+          type="submit"
+          className="btn btn-lg btn-primary btn-dark mb-5"
+        >
+          Update
+        </button>
+      </form>
+    </>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const {
+    isFetching,
+    username,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    age,
+    job,
+    university,
+    introduction_method,
+    gender,
+    city,
+    country,
+    field_of_interest,
+    grade,
+    is_student,
+    favorite_tags,
+  } = state.Participant;
+  return {
+    isFetching,
+    username,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    age,
+    job,
+    university,
+    introduction_method,
+    gender,
+    city,
+    country,
+    field_of_interest,
+    grade,
+    is_student,
+    favorite_tags,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    getProfile,
+    updateProfile,
+  })(Profile);
