e373b93f2b7f749dbf52a82718f466f61219ec1c
LordLoss <a.a.ghanati@gmail.com>
2020-12-13 23:54:14 +0330

    wrote a wort function for staff page

--- front/src/views/Staff.tsx
+++ front/src/views/Staff.tsx
@@ -33,7 +33,52 @@ const Staff = ({
               {staff.length > 0 && !isFetching &&
                 <div className="row">
                   {
-                    staff.filter((staff) => team.staff.includes(staff.id)).sort((staff) => staff.id).map((staff) => (
+                    staff.filter((staff) => team.staff.includes(staff.id)).sort((staff) => {
+                      if (team.name === 'Content') {
+                        if (staff.name === 'Mahdi Farvardin') return 1
+                        else if (staff.name === 'Hossein Firooz') return 2
+                        else if (staff.name === 'Sepehr Amini Afshar') return 3
+                        else if (staff.name === 'Farzam Zohdinasab') return 4
+                        else if (staff.name === 'Pooya Moeini') return 5
+                        else if (staff.name === 'Seyed Mohammad mehdi Hatami') return 6
+                      } else if (team.name === 'Technical') {
+                        if (staff.name === 'Emran Batmanghelich') return 1
+                        else if (staff.name === 'Ahmad Salimi') return 2
+                        else if (staff.name === 'Ali asghar Ghanati') return 3
+                        else if (staff.name === 'Fatemeh Khashei') return 4
+                        else if (staff.name === 'Alireza Tajmir riahi') return 5
+                        else if (staff.name === 'Seyed Alireza Hashemi') return 6
+                        else if (staff.name === 'Mohammad mehdi Barghi') return 7
+                      } else if (team.name === 'Networking') {
+                        if (staff.name === 'Amirhossein Hadian') return 1
+                        else if (staff.name === 'Shima Ramadani') return 2
+                        else if (staff.name === 'Sara Azarnoosh') return 3
+                        else if (staff.name === 'Ehsan Movafagh') return 4
+                        else if (staff.name === 'Fatemeh Asgari') return 5
+                        else if (staff.name === 'Sajjad Rezvani') return 6
+                        else if (staff.name === 'Amirmohammad Imani') return 7
+                        else if (staff.name === 'Mehdi Jalali') return 8
+                      } else if (team.name === 'Social') {
+                        if (staff.name === 'Sara Azarnoosh') return 1
+                        else if (staff.name === 'Dorna Dehghani') return 2
+                        else if (staff.name === 'Ghazal Shenavar') return 3
+                        else if (staff.name === 'Helia Akhtarkavian') return 4
+                        else if (staff.name === 'Sabiheh Tajdari') return 5
+                        else if (staff.name === 'Sahel Messforoosh') return 6
+                        else if (staff.name === 'Esmaeil Pahang') return 7
+                      } else if (team.name === 'Media') {
+                        if (staff.name === 'Hamila Meili') return 1
+                        else if (staff.name === 'Mahdieh Ebrahimpoor') return 2
+                        else if (staff.name === 'Roya Ghavami') return 3
+                        else if (staff.name === 'Sara Zahedi') return 4
+                        else if (staff.name === 'Mohammad mehdi Barghi') return 5
+                      } else if (team.name === 'Presentation Management') {
+                        if (staff.name === 'Alireza Ziaei') return 1
+                        else if (staff.name === 'Sajjad Rezvani') return 2
+                        else if (staff.name === 'Vahid Zehtab') return 3
+                        else if (staff.name === 'Amirhossein Asem Yousefi') return 4
+                      }
+                    }).map((staff) => (
                       <div key={staff.id} className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
                         <PublicCard id={staff.id} isStaff='true'></PublicCard>
                       </div>
