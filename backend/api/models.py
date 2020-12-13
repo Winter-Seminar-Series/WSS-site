979b0997dd16bd4b581164533aec1986407269b6
Alireza Tajmir <45307044+AlirezaT99@users.noreply.github.com>
2020-12-13 12:53:21 +0330

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,12 +15,12 @@ or be imported in ready() function in apps.py '''
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
     base_url = 'https://sharif-wss.ir/password-reset/confirm'
-    email_message = "{}/{}".format(base_url, reset_password_token.key)
+    reset_password_link = "{}/{}".format(base_url, reset_password_token.key)
 
     send_mail(
         RESET_PASSWORD_SUBJECT, 'text content',
         settings.EMAIL_HOST_USER,
         [reset_password_token.user.email],
         fail_silently=True,
-        html_message=BASE_HTML_CONTENT.format(RESET_PASSWORD_EMAIL.format(email_message))
+        html_message=BASE_HTML_CONTENT.format(RESET_PASSWORD_EMAIL.format(reset_password_link))
     )
