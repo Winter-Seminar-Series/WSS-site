257b071a02a9afba1a3f7e242d91dc5476e970b2
ahmadsalimi <ahsa9978@gmail.com>
2020-12-15 12:55:26 +0330

    remove old views at all

--- backend/WSS_Site/error_handler_views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-
-from WSS.mixins import FooterMixin
-
-
-class Handler404View(FooterMixin, TemplateView):
-    template_name = '404.html'  # It seems a bit dirty:)))
--- backend/WSS_Site/urls.py
+++ backend/WSS_Site/urls.py
@@ -3,19 +3,12 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from WSS_Site.error_handler_views import Handler404View
-
 urlpatterns = [
                   url(r'^jet/', include('jet.urls', 'jet')),
                   url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
                   url(r'^admin-of-wss/', admin.site.urls),
-                  url(r'^', include(('WSS.urls', 'WSS'), namespace='wss')),
-                  url(r'^', include(('events.urls', 'events'), namespace='events')),
-                  url(r'^', include(('people.urls', 'people'), namespace='people')),
                   url(r'^api/', include(('api.urls', 'api'), namespace='api')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'WSS Administration'
 admin.site.site_title = 'WSS Administration'
-
-handler404 = Handler404View.as_view()
