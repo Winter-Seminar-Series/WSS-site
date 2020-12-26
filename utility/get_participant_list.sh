549c3908fb3ae24c0544bdb94f4d3d141330d025
ahmadsalimi <ahsa9978@gmail.com>
2020-12-26 23:33:46 +0330

    update

--- utility/get_participant_list.sh
+++ utility/get_participant_list.sh
@@ -13,8 +13,22 @@ content_type="Content-Type: application/json"
 login_data="{\"username\": \"$username\", \"password\": \"$password\"}"
 
 echo "Logging in as $username"
-login_response=$(curl -X POST -H "$content_type" -d "$login_data" ${api_url}/login/)
-token=$(python -c "import json; print(json.loads('$login_response')['token'])")
+login_response=$(curl -X POST -H "$content_type" -d "$login_data" -s ${api_url}/login/)
+
+token=$(python <<EOF
+import json
+response = json.loads('$login_response')
+if 'token' in response:
+    print(response['token'])
+EOF
+)
+
+if [ -z "$token" ]; then
+    echo $login_response
+    exit 1
+fi
+
+echo "Successfull login"
 
 authorization_header="Authorization: Token $token"
 
@@ -24,6 +38,6 @@ curl -X GET -H "$authorization_header" -H "Accept: text/csv" ${api_url}/2020/par
 
 echo "Logging out..."
 
-curl -X POST -H "$authorization_header" ${api_url}/logout/
+curl -X POST -H "$authorization_header" -s ${api_url}/logout/
 
 echo "Done!"
