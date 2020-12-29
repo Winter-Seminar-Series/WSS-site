600a31344bcb5804f8a46ca5582d66c51db95a68
mohammad mahdi barghi <61848164+mmahdibarghi@users.noreply.github.com>
2020-12-29 08:13:52 -0500

    cards are complitly linked (#232)
    * cards are complitly linked
    
    * fix link
    
    * center cards
    
    Co-authored-by: mmahdibarghi <mmahdibarghi7980@gmail.com>
    Co-authored-by: Hamed Alimohammadzadeh <33548307+Hamedamz@users.noreply.github.com>
    Co-authored-by: Hamed AMZ <amzhamed@gmail.com>

--- front/src/views/Staff.tsx
+++ front/src/views/Staff.tsx
@@ -25,13 +25,23 @@ const Staff = ({
           <div className="row mb-3">
             <h2 className="mb-1 col section-sub-title title-white">Staff</h2>
           </div>
+          {isFetching &&
+          <div className="row">
+            <div className="col mb-3 text-center">Loading...</div>
+          </div>
+          }
+          {staff.length === 0 && !isFetching &&
+          <div className="row">
+            <div className="col text-center">Nothing has been added yet</div>
+          </div>
+          }
           {holding_teams.map((team) => (
             <>
               <div className="row mb-3">
-                <h3 className="mb-2 mt-3 col white">{team.name}</h3>
+                <h3 className="mb-2 mt-4 col white text-center">{team.name}</h3>
               </div>
               {staff.length > 0 && !isFetching &&
-                <div className="row">
+                <div className="row justify-content-center">
                   {
                     team.staff.map((staff) => (
                       <div key={staff} className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
@@ -39,16 +49,6 @@ const Staff = ({
                       </div>
                     ))
                   }
-                </div>
-              }
-              {isFetching && 
-                <div className="row">
-                  <div className="col mb-3">Loading...</div>
-                </div>
-              }
-              {staff.length == 0 && !isFetching &&
-                <div className="row">
-                  <div className="col">Nothing has been added yet</div>
                 </div>
               }
             </>
