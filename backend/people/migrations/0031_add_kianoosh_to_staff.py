148334c75e89b843d15bf7072f58d74c4a7af52c
Alireza Tajmir <45307044+AlirezaT99@users.noreply.github.com>
2021-01-05 12:22:24 +0330

    Added Kianoosh migration (#237)
    * Added migration
    
    * Corrected order

---
+++ backend/people/migrations/0031_add_kianoosh_to_staff.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+from django.db import migrations
+from django.db.models import F
+
+
+def forwards(apps, schema_editor):
+    Staff = apps.get_model("people", "Staff")
+    HoldingTeam = apps.get_model("people", "HoldingTeam")
+    ContentType = apps.get_model("contenttypes", "ContentType")
+
+    ctype = ContentType.objects.get_for_model(Staff)
+
+    presentation_team = HoldingTeam.objects.get(
+        wss__year=2020, name="Presentation Management"
+    )
+
+    new_staff = Staff.objects.create(
+        name="Kianoosh Abbasi",
+        picture="media/2020/staff/Kianoosh-Abbasi.jpg",
+        polymorphic_ctype=ctype,
+        order=6,
+    )
+    presentation_team.staff.add(new_staff)
+
+
+def rollback(apps, schema_editor):
+    HoldingTeam = apps.get_model("people", "HoldingTeam")
+
+    presentation_team = HoldingTeam.objects.get(
+        wss__year=2020, name="Presentation Management"
+    )
+    presentation_team.staff.filter(name="Kianoosh Abbasi").delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("people", "0030_add_and_update_speakers"),
+    ]
+
+    operations = [migrations.RunPython(forwards, rollback)]
