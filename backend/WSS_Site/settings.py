68e3831c3b45edbffe6b50a11ad10dd58dc4ab7b
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2022-01-27 10:33:50 +0330

    Change cors allowed origins

--- backend/WSS_Site/settings.py
+++ backend/WSS_Site/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('WSS_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost"
+]
 
 # Application definition
 
