659f60a9568d09c4980723fb25afa6e0ea641913
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-01-27 11:02:53 +0330

    - something I forgot :)

--- front/src/redux/actions/urls.tsx
+++ front/src/redux/actions/urls.tsx
@@ -3,7 +3,7 @@ import { BASE_URL } from '../../constants/info'
 export const ROOT =
   process.env.NODE_ENV === 'production'
     ? BASE_URL.concat('/api/')
-    : 'http://localhost/api/';
+    : 'https://localhost/api/';
 
 export const REGISTER = ROOT.concat('register/');
 export const LOGIN = ROOT.concat('login/');
