2aeb8adc0f087a8d3a655bac7391d34dc3b8cbc5
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-01-27 11:02:07 +0330

    - fixed some minor things

--- front/src/redux/actions/urls.tsx
+++ front/src/redux/actions/urls.tsx
@@ -3,9 +3,8 @@ import { BASE_URL } from '../../constants/info'
 export const ROOT =
   process.env.NODE_ENV === 'production'
     ? BASE_URL.concat('/api/')
-    : 'https://cors-anywhere.herokuapp.com/https://sharif-wss.ir/api/';
+    : 'http://localhost/api/';
 
-// export const ROOT = BASE_URL.concat('/api/')
 export const REGISTER = ROOT.concat('register/');
 export const LOGIN = ROOT.concat('login/');
 export const LOGOUT = ROOT.concat('logout/');
