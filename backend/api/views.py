1481a7acc855836fa25e555a2335a5976f737d6f
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-02-08 10:52:08 +0330

    - added a few more fields to userprofile

--- backend/api/views.py
+++ backend/api/views.py
@@ -477,7 +477,7 @@ class PaymentViewSet(viewsets.ViewSet):
                 "message": "You already have finished your payment."
             })
         elif ((not user_profile.grade) or (not user_profile.email) or (not user_profile.phone_number)
-              or (not user_profile.job) or (not user_profile.university)):
+              or (not user_profile.job) or (not user_profile.university)):  # TODO
             return ErrorResponse({
                 "message": "Some required fields are blank."
             })
