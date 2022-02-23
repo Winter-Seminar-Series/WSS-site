41f7c31a0c32bfbebc37481fa19133b1e4a26be2
MohammadHossein Dolatabadi <58592602+mhdolatabadi@users.noreply.github.com>
2022-02-24 03:08:13 +0330

    Add skyroom url (#310)
    * fix(SeminarDetails): uncomment seminar times
    
    * fix(participant): get redirect url body
    
    * Edit toast message
    
    Co-authored-by: Soroush Jahanzad <SoroushJahanzad@gmail.com>
    Co-authored-by: SJahanzad <32274778+SJahanzad@users.noreply.github.com>

--- front/src/components/GoToButton.tsx
+++ front/src/components/GoToButton.tsx
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { getRedirectURL } from '../redux/actions/participant';
 
-function GoToButton({ thisSeries, type, id, getRedirectURL }) {
+function GoToButton({ thisSeries, type, id, getRedirectURL, room_name }) {
   const [loading, setLoading] = useState(false);
 
   async function redirectToClass() {
     setLoading(true);
 
     try {
-      const { response } = await getRedirectURL(thisSeries, type, id);
+      const { response } = await getRedirectURL(
+        thisSeries,
+        type,
+        id,
+        room_name
+      );
 
       if (response && response.redirect_url) {
         window.open(response.redirect_url, '_blank');
