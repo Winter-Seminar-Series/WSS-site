c2bbed74c51429bf03cd3a3b2d89686913d0a59b
AliLordLoss <45513491+AliLordLoss@users.noreply.github.com>
2022-02-24 21:37:23 +0330

    Update roundtables (#321)
    * updated some fields of roundtables and their speakers
    
    * changed speakers of some roundtables

---
+++ backend/events/migrations/0043_update_some_roundtable_speakers.py
@@ -0,0 +1,51 @@
+from django.db import migrations
+
+
+def forwards(apps, schema_editor):
+    from datetime import datetime, timedelta
+    RoundTable = apps.get_model('events', 'RoundTable')
+    WSS = apps.get_model('WSS', 'WSS')
+    Speaker = apps.get_model('people', 'Speaker')
+
+    wss = WSS.objects.get(year=2021)
+
+    rt = RoundTable.objects.filter(
+        wss=wss, subject='Ph.D. student\'s life - Academic life')
+    rt.speakers.remove(Speaker.objects.filter(name='Amir Moradi').last())
+    rt.save()
+
+    rt = RoundTable.objects.filter(
+        wss=wss, subject='Which one is for me? Masters or Ph.D.')
+    rt.speakers.add(Speaker.objects.filter(name='Arash Pourdamghani').last())
+    rt.save()
+
+
+def rollback(apps, schema_editor):
+    WSS = apps.get_model('WSS', 'WSS')
+    RoundTable = apps.get_model('events', 'RoundTable')
+    Speaker = apps.get_model('people', 'Speaker')
+
+    wss = WSS.objects.get(year=2021)
+
+    rt = RoundTable.objects.filter(
+        wss=wss, subject='Ph.D. student\'s life - Academic life')
+    rt.speakers.add(Speaker.objects.filter(name='Amir Moradi').last())
+    rt.save()
+
+    rt = RoundTable.objects.filter(
+        wss=wss, subject='Which one is for me? Masters or Ph.D.')
+    rt.speakers.remove(Speaker.objects.filter(
+        name='Arash Pourdamghani').last())
+    rt.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('people', '0043_add_a_new_roundtable_speaker'),
+        ('events', '0042_fix_roundtable_data'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, rollback)
+    ]
