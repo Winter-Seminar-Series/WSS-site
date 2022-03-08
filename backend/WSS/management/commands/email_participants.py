4fa041aae02ffcf928add5114cd5ba665465454e
Ali asghar Ghanati <a.a.ghanati@gmail.com>
2022-03-08 15:35:08 +0330

    - added staff emails

--- backend/WSS/management/commands/email_participants.py
+++ backend/WSS/management/commands/email_participants.py
@@ -17,7 +17,45 @@ class Command(BaseCommand):
         wss = WSS.objects.get(year=year)
 
         ps = [('a.a.ghanati@gmail.com', 'علی'),
-              ('farzamzohdi@gmail.com', 'فرزام')]
+              ('farzamzohdi@gmail.com', 'فرزام'),
+              ('sheiramed@gmail.com', 'Sheima Ramedani'),
+              ('arahmizad@gmail.com', 'Ali Rahmizad'),
+              ('SoroushJahanzad@gmail.com', 'Soroush Jahanzad'),
+              ('arshiamoghimi98@gmail.com', 'Arshia Moghimi'),
+              ('pooyayou@gmail.com', 'Pooya Yousefi'),
+              ('yasmin.kadkhodaei@gmail.com', 'Yasmin Kadkhodaei'),
+              ('arian_z@rocketmail.com', 'arian zamani'),
+              ('Mehrad_Milanloo@yahoo.com', 'Mehrad Milanloo'),
+              (' hssoleymanmh@gmail.com', 'Mohammad Hossein Soleymani'),
+              ('amirm137878@gmail.com', 'Amirmahdi Namjoo'),
+              ('kkibian@gmail.com', 'Kian Bahadori'),
+              ('mahdi.farvardin@gmail.com', 'Mahdi Farvardin'),
+              ('imanm1381@gmail.com', 'Iman Mohammadi'),
+              ('s.nazari80820@gmail.com', 'Soheil Nazari'),
+              ('m.h.dolatabadi.a@gmail.com ', 'محمدحسین دولت‌آبادی '),
+              ('ali.javanmard14@gmail.com ', 'علی جوانمرد'),
+              ('ali.javanmard14@gmail.com ', 'Ali Javanmard '),
+              ('mahdi_b7978@yahoo.com', 'MohammadMahdi Barghi'),
+              ('dnya.nvb@gmail.com', 'Donya Navabi'),
+              ('mhmmoshtaghi@gmail.com', 'Mohammad Moshtaghi'),
+              ('Paradisez2001@gmail.com ', 'Pardis Zahraei'),
+              ('ssrahimi2@gmail.com', 'سیاوش رحیمی شاطرانلو'),
+              ('Tajdari996@gmail.com ', 'Sabiha Tajdari '),
+              ('hyousefi1379@gmail.com ', ''),
+              ('alizademhdi@gmail.com', 'mahdi alizade'),
+              ('hesamsoleymani20@gmail.com', 'Hesam Soleymani'),
+              ('behzadnabawi@gmail.com', 'Behzad Nabavi'),
+              ('hastikarimi2002@gmail.com ', 'Hasti Karimi'),
+              ('sarazm.2000@gmail.com', 'Sara Zahedi'),
+              ('reihane.zohrabi@gmail.com', 'Reihane Zohrabi'),
+              ('babashahnegar@gmail.com', 'Negar Babashah'),
+              ('mohammad.abolnejadian@gmail.com', 'محمد ابول نژاديان '),
+              ('farzamzohdi@gmail.com', 'farzam'),
+              ('mobina.salimipanah@gmail.com', 'mobina'),
+              ('mohammadali1379@gmail.com', 'MohammadAli MohammadKhani'),
+              ('royadaneshi2001@gmail.com', 'Roya Daneshi'),
+              ('sorousherafat@gmail.com', 'Soroush Sherafat'),
+              ]
         for p in ps:
             print(p)
             send_mail(
@@ -26,7 +64,7 @@ class Command(BaseCommand):
                 [p[0]],
                 fail_silently=False,
                 html_message=PERSIAN_BASE_HTML_CONTENT.format(
-                    SURVEY_EMAIL.format(
+                    STAFF_EMAIL.format(
                         p[1])
                 )
             )
