049d5c1bf44a71d8f46a8b3e0fc27d04d020c4ea
mahdilotfi167 <mahdi.vahshat.80@gmail.com>
2023-03-18 13:08:34 +0330

    Convert 7th to 8th in consts

--- backend/templates/consts.py
+++ backend/templates/consts.py
@@ -6,7 +6,7 @@ RAW_BASE_HTML = '''<!DOCTYPE html>
     <head>
         <meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
         <meta name="referrer" content="no-referrer">
-    <title>7th WSS</title>
+    <title>8th WSS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <style>
@@ -100,14 +100,14 @@ RAW_BASE_HTML = '''<!DOCTYPE html>
       <div>
           <div>
               <div id="logo" class="text-center p-relative px-1 py-1">
-                <a href="http://wss.ce.sharif.edu" target="_blank" style="text-decoration: none;"><img src="https://wss.ce.sharif.edu/media/7th/logos/banner1.png" style="width: 100%" border="0" alt="WSS 7th Series"></a>
+                <a href="https://wss.ce.sharif.edu" target="_blank" style="text-decoration: none;"><img src="https://wss.ce.sharif.edu/images/headerlogo.png" style="width: 100%" border="0" alt="WSS 8th Series"></a>
               </div>
           </div>
           {{{content_to_be_injected}}}
           <footer>
             <div class="text-center px-1 py-1">
                 <div class="py-2 p-relative" id="site">
-                    <a href="http://wss.ce.sharif.edu" target="_blank" style="text-decoration: none;"><img src="https://s17.picofile.com/file/8414284650/wss_ce_sharif_edu.png" border="0" width="137.6" height="13.6" alt="wss.ce.sharif.edu"></a>
+                    <a href="https://wss.ce.sharif.edu" target="_blank" style="text-decoration: none;"><img src="https://s17.picofile.com/file/8414284650/wss_ce_sharif_edu.png" border="0" width="137.6" height="13.6" alt="wss.ce.sharif.edu"></a>
                 </div>
                 <div class="py-2">
                 <a href="https://t.me/wss_sut" target="_blank" class="px-2" style="text-decoration: none;"><img src="https://wss.ce.sharif.edu/media/7th/logos/Telegram.png" border="0" width="26" height="25" style="width: 26px; height: 25px; max-width: 26px;border:none; outline:none; text-decoration:none;font-size:19px;font-family:URWGeometric,Helvetica Neue, Arial, sans-serif;color:#00aced;font-weight:bold" alt="   te"></a>
@@ -137,7 +137,7 @@ PERSIAN_BASE_HTML_CONTENT = RAW_BASE_HTML.replace("{{{content_to_be_injected}}}"
 
 PAYMENT_EMAIL = (
   "Dear {},<br>"
-  "Thanks for your interest in attending 7th WSS <br>"
+  "Thanks for your interest in attending 8th WSS <br>"
   "Your payment was successful. <br>"
   "Ref code: {} <br>"
 )
