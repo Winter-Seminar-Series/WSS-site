a9c13721a7e5f1fe24d0c697fa8be66e9057a17b
Negarnbkhti42 <norob35@gmail.com>
2023-04-01 15:23:08 +0330

    Created network card

---
+++ front/src/components/cards/NetworkCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+
+function NetworkCard({ id, image, name, lastName, position, association }) {
+  return (
+    <Card
+      sx={{ width: '273px', boxShadow: 'none', display: 'flex', gap: '10px' }}>
+      <CardMedia
+        component="img"
+        image={image}
+        sx={{ width: 112, height: 112, borderRadius: '0.75rem' }}
+      />
+      <CardContent
+        sx={[
+          {
+            padding: '0.69rem 0',
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          },
+          { '&:last-child': { paddingBottom: '0.94rem' } },
+        ]}>
+        <Typography
+          sx={{
+            // textDecoration: 'underline',
+            borderBottom: '1.72px solid #638932',
+            fontSize: '0.97rem',
+            fontWeight: '700',
+          }}>
+          {name} {lastName}
+        </Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '0.44rem' }}>
+          <Typography sx={{ fontSize: '0.77rem' }}>{position}</Typography>
+          <Typography sx={{ fontSize: '0.77rem' }}>at {association}</Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default NetworkCard;
--- front/src/views/NetworkHall.tsx
+++ front/src/views/NetworkHall.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import NetworkCard from '../components/cards/NetworkCard';
+
+const NETWORK = {
+  id: 1,
+  name: 'Name',
+  lastName: 'Lastname',
+  position: 'Associate professor',
+  image: 'https://thinksport.com.au/wp-content/uploads/2020/01/avatar-.jpg',
+  association: 'Oxford University',
+};
 
 function NetworkHall() {
-  return <div></div>;
+  return (
+    <section className="pt-4">
+      <NetworkCard {...NETWORK} />
+    </section>
+  );
 }
 
 export default NetworkHall;
