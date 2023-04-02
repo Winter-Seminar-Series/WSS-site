fbaa59a39d7eb12b9de258d8fd8ad45af87114ff
Soroush Sherafat <71870584+sorousherafat@users.noreply.github.com>
2023-04-03 00:05:29 +0330

    Page background (#379)
    * made seminar background cover and fixed
    
    * made round table background cover and fixed
    
    * made labtalk background cover and fixed

--- front/src/views/Staff.tsx
+++ front/src/views/Staff.tsx
@@ -18,44 +18,44 @@ const Staff = ({
 
   return (
     <>
-    <div className='fixed-background'>
-      <section
-        id="ts-speakers"
-        className="ts-speakers diagonal">
-        <div className="container text-white">
-          <div className="row mb-3">
-            <h2 className="mb-1 col section-sub-title title-white">Staff</h2>
-          </div>
-          {isFetching && (
-            <div className="row">
-              <div className="col mb-3 text-center">Loading...</div>
+      <div className="fixed-background">
+        <section
+          id="ts-speakers"
+          className="ts-speakers diagonal">
+          <div className="container text-white">
+            <div className="row mb-3">
+              <h2 className="mb-1 col section-sub-title title-white">Staff</h2>
             </div>
-          )}
-          {staff.length === 0 && !isFetching && (
-            <div className="row">
-              <div className="col text-center">Nothing has been added yet</div>
-            </div>
-          )}
-          {holding_teams.map((team) => (
-            <>
-              <div className="row mb-3">
-                <h3 className="mb-2 mt-4 col white text-center">{team.name}</h3>
+            {isFetching && (
+              <div className="row">
+                <div className="col mb-3 text-center">Loading...</div>
               </div>
-              {staff.length > 0 && !isFetching && (
-                <div className="row justify-content-center">
-                  {team.staff.map((staff) => (
-                    <div
-                      key={staff}
-                      className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
-                      <PublicCard id={staff} isStaff="true"></PublicCard>
-                    </div>
-                  ))}
+            )}
+            {staff.length === 0 && !isFetching && (
+              <div className="row">
+                <div className="col text-center">Nothing has been added yet</div>
+              </div>
+            )}
+            {holding_teams.map((team) => (
+              <>
+                <div className="row mb-3">
+                  <h3 className="mb-2 mt-4 col white text-center">{team.name}</h3>
                 </div>
-              )}
-            </>
-          ))}
-        </div>
-      </section>
+                {staff.length > 0 && !isFetching && (
+                  <div className="row justify-content-center">
+                    {team.staff.map((staff) => (
+                      <div
+                        key={staff}
+                        className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
+                        <PublicCard id={staff} isStaff="true"></PublicCard>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
