3261d481f2e9dff9656b9ee0e2695caf878352be
Soroush Sherafat <sorousherafat@gmail.com>
2023-04-05 03:01:25 +0330

    finished `Streams` and created `ViewStream`

--- front/src/views/Dashboard/Streams.tsx
+++ front/src/views/Dashboard/Streams.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import SeminarCard from '../../components/cards/SeminarCard';
 import PublicCard from '../../components/cards/PublicCard';
 import RoundTableCard from '../../components/cards/RoundTableCard';
 import LabTalkCard from '../../components/cards/LabTalkCard';
-import { getModelList, MODEL_LISTS_NAMES } from '../../redux/actions/WSS';
+import { getModelList, getWSSPrimitiveFields, MODEL_LISTS_NAMES } from '../../redux/actions/WSS';
 
 const Streams = ({
-  thisSeries,
-  getWSSPrimitiveFields,
   getModelList,
+  getWSSPrimitiveFields,
+  thisSeries,
   streams,
-  speakers,
   seminars,
   workshops,
   roundtables,
   labtalks,
   isFetching,
 }) => {
+
   useEffect(() => {
     getModelList(MODEL_LISTS_NAMES.STREAMS, thisSeries);
     getModelList(MODEL_LISTS_NAMES.SPEAKERS, thisSeries);
@@ -27,59 +27,60 @@ const Streams = ({
     getModelList(MODEL_LISTS_NAMES.LAB_TALKS, thisSeries);
   }, [getWSSPrimitiveFields]);
 
-  useEffect(() => {
-  }, [streams])
-
   const mapEventTypeToComponent = {
     "seminar": (event, index) => {
       const seminar = seminars.find((seminar) => seminar.id === event.id);
+      if (!seminar) return;
       return (
         <div
-          key={index}
+          key={seminar.id}
           className="col-xs-10 col-sm-6 col-lg-3 mt-2 mb-4">
           <SeminarCard
             id={seminar.speaker}
             poster_picture={seminar.poster_picture}
-            presentationLink={'/seminar/' + seminar.id}
+            presentationLink={'/dashboard/stream/' + seminar.id}
           />
         </div>
       );
     },
     "workshop": (event, index) => {
       const workshop = workshops.find((workshop) => workshop.id === event.id);
+      if (!workshop) return;
       return (
         <div
-          key={index}
+          key={workshop.id}
           className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
           <PublicCard
             id={workshop.speaker}
-            presentationLink={'/workshop/' + workshop.id}
+            presentationLink={'/dashboard/stream/' + workshop.id}
           />
         </div>
       );
     },
     "roundtable": (event, index) => {
       const roundTable = roundtables.find((roundTable) => roundTable.id === event.id);
+      if (!roundTable) return;
       return (
         <div
-          key={index}
+          key={roundTable.id}
           className="col-xs-10 col-sm-6 col-lg-3 mt-2 mb-4">
           <RoundTableCard
             id={roundTable.id}
-            presentationLink={'/roundtable/' + roundTable.id}
+            presentationLink={'/dashboard/stream/' + roundTable.id}
           />
         </div>
       );
     },
     "labtalk": (event, index) => {
       const labTalk = labtalks.find((labTalk) => labTalk.id === event.id);
+      if (!labTalk) return;
       return (
         <div
-          key={index}
+          key={labTalk.id}
           className="col-xs-10 col-sm-6 col-lg-3 mt-2 mb-4">
           <LabTalkCard
             id={labTalk.id}
-            presentationLink={'/labtalk/' + labTalk.id}
+            presentationLink={'/dashboard/stream/' + labTalk.id}
           />
         </div>
       );
@@ -88,59 +89,67 @@ const Streams = ({
 
   return (
     <>
-      <div className="fixed-background">
-        <section
-          id="ts-speakers"
-          className="ts-speakers pt-4">
-          <div className="container text-white">
-            <div className="row mb-3">
-              <h3 className="mb-1 col section-sub-title title-white">Seminars</h3>
-            </div>
-            {streams.length > 0 && !isFetching && (
-              <div className="row">
-                {
-                  // seminars.map((seminar) => (
-                  //   <div key={seminar.id} className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
-                  //     <PublicCard id={seminar.speaker} presentationLink={'/seminar/' + seminar.id}></PublicCard>
-                  //   </div>
-                  // ))
-                  Array.from(Array(streams.length).keys())
-                    .sort(() => Math.random() - 0.5)
-                    .map((index) => {
-                      const event = streams[index];
-                      const eventComponent = event && mapEventTypeToComponent[event.type];
-                      return eventComponent && eventComponent(streams[index], index)
-                    })
-                }
-              </div>
-            )}
-            {isFetching && (
-              <div className="row">
-                <div className="col">Loading...</div>
-              </div>
-            )}
-            {streams.length === 0 && !isFetching && (
-              <div className="row">
-                <div className="col">No event is currently streaming</div>
-              </div>
-            )}
-          </div>
-        </section>
+      <div className="diagonal seminar-register-title background-theme d-flex align-items-center">
+        <div className="header ml-3 pt-5">
+          <div className="title">Streaming Events</div>
+        </div>
+      </div>
+      <div className="container py-5 my-5">
+      {streams.length > 0 && !isFetching && (
+        <div className="row">
+          {
+            // seminars.map((seminar) => (
+            //   <div key={seminar.id} className="col-xs-12 col-sm-6 col-lg-3 mt-2 mb-4">
+            //     <PublicCard id={seminar.speaker} presentationLink={'/seminar/' + seminar.id}></PublicCard>
+            //   </div>
+            // ))
+            Array.from(Array(streams.length).keys())
+              .sort(() => Math.random() - 0.5)
+              .map((index) => {
+                const event = streams[index];
+                const eventComponent = event && mapEventTypeToComponent[event.type];
+                return eventComponent && eventComponent(streams[index], index)
+              })
+          }
+        </div>
+      )}
+      {isFetching && (
+        <div className="row">
+          <div className="col">Loading...</div>
+        </div>
+      )}
+      {streams.length === 0 && !isFetching && (
+        <div className="row">
+          <div className="col">No event is currently streaming</div>
+        </div>
+      )}
       </div>
     </>
   );
 };
 
 const mapStateToProps = (state, ownProps) => {
-  const { isFetching, seminars } = state.WSS;
+  const {
+    isFetching,
+    streams,
+    seminars,
+    workshops,
+    roundtables,
+    labtalks
+  } = state.WSS;
   const { thisSeries } = state.account;
   return {
     thisSeries,
-    isFetching,
+    streams,
     seminars,
+    workshops,
+    roundtables,
+    labtalks,
+    isFetching,
   };
 };
 
 export default connect(mapStateToProps, {
   getModelList,
+  getWSSPrimitiveFields
 })(Streams);
