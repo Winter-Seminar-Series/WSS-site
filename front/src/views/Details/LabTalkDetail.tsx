3a94f5dd9eb04d0731f6e96a19bd0288e15bdb9f
NegarAskari <79265117+NegarAskari@users.noreply.github.com>
2023-04-06 03:35:05 +0330

    added google form buttons (#390)

--- front/src/views/Details/LabTalkDetail.tsx
+++ front/src/views/Details/LabTalkDetail.tsx
@@ -28,6 +28,7 @@ function LabTalkDetail({
     tags: [],
     website_link: '',
     poster_picture: '',
+    form_url: '',
   });
   const [head, setHead] = useState({
     picture: '',
@@ -150,6 +151,11 @@ function LabTalkDetail({
                     room_name={'labtalk'}
                   />
                 )}
+                {labTalk.form_url && 
+                <button type = 'button' className = 'btn btn-primary ml-2'
+                onClick={() => window.open(labTalk.form_url)}>
+                  Google Form
+                </button>}
               </div>
             </div>
           </div>
