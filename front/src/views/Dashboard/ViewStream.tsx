ed5e2b8b9566e6b824dba0204bac2b2ed97c6a12
Soroush Sherafat <sorousherafat@gmail.com>
2023-04-07 14:04:12 +0330

    added title to `ViewStream`

--- front/src/views/Dashboard/ViewStream.tsx
+++ front/src/views/Dashboard/ViewStream.tsx
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { getWSSPrimitiveFields, getModelList, MODEL_LISTS_NAMES } from '../../redux/actions/WSS';
+import { getWSSPrimitiveFields, getModelList, getAnEntityOfModelList, MODEL_LISTS_NAMES } from '../../redux/actions/WSS';
 
 const ViewStream = ({
   streams,
+  seminars,
+  roundTables,
+  labTalks,
   isFetching,
   thisSeries,
   getWSSPrimitiveFields,
   getModelList,
+  getAnEntityOfModelList,
 }) => {
   const id = parseInt(useParams()['id']);
+
+  const mapStreamTypeToModelName = {
+    "seminar": MODEL_LISTS_NAMES.SEMINARS,
+    "roundtable": MODEL_LISTS_NAMES.ROUND_TABLES,
+    "labtalk": MODEL_LISTS_NAMES.LAB_TALKS,
+  };
+
+  const mapStreamTypeToEvents = {
+    "seminar": seminars,
+    "roundtable": roundTables,
+    "labtalk": labTalks,
+  };
+
   const [stream, setStream] = useState({
     id: 0,
     type: '',
+    title: '',
     stream_room: {
       tag: '',
       url: '',
@@ -23,8 +41,29 @@ const ViewStream = ({
   });
 
   useEffect(() => {
+    if (!stream?.id) return;
+    const modelName = mapStreamTypeToModelName[stream?.type ?? ""];
+    if (!modelName) return;
+
+    getAnEntityOfModelList(modelName, thisSeries, stream.id);
+  }, [getWSSPrimitiveFields, stream]);
+
+  useEffect(() => {
     getModelList(MODEL_LISTS_NAMES.STREAMS, thisSeries);
   }, [getWSSPrimitiveFields]);
+
+  useEffect(() => {
+    if (!stream?.id) return;
+    const events = mapStreamTypeToEvents[stream?.type ?? ""];
+    if (!events) return;
+    const event = events.find((e) => e.id === stream.id);
+    if (!event?.title) return;
+
+    setStream({
+      ...stream,
+      title: event.title,
+    });
+  }, [seminars, roundTables, labTalks]);
 
   useEffect(() => {
     const stream = streams.find((s) => s.id === id);
@@ -34,9 +73,6 @@ const ViewStream = ({
 
   return (
     <>
-      {isFetching && (
-        <span>Loading...</span>
-      )}
       {!stream?.stream_room?.tag && stream?.type !== "workshop" && !isFetching && (
         <div style={{ "margin": "auto" }} className="py-5 text-center">
           An error ocurred.
@@ -48,11 +84,12 @@ const ViewStream = ({
         </div>
       )}
       {stream?.stream_room?.tag && stream?.type !== "workshop" && (
-        <div style={{ "width": "50%", "margin": "auto" }} className="py-5">
+        <div style={{ "width": "50%", "margin": "auto" }} className="text-center py-2">
+          <h2 className="pb-2">{stream.title}</h2>
           <div dangerouslySetInnerHTML={{ __html: stream.stream_room.tag }} />
         </div>
       )}
-      <div className="d-flex" style={{ "width": "20%", "margin": "auto" }} >
+      <div className="d-flex pt-2" style={{ "width": "20%", "margin": "auto" }} >
         {stream?.qa_url &&
           <div className="col text-center">
             <button type='button' className='btn btn-primary'
@@ -76,13 +113,19 @@ const mapStateToProps = (state) => {
   const {
     isFetching,
     streams,
+    seminars,
+    roundtables: roundTables,
+    labtalks: labTalks,
   } = state.WSS;
   const { thisSeries } = state.account;
   return {
     thisSeries,
     streams,
+    seminars,
+    roundTables,
+    labTalks,
     isFetching,
   };
 };
 
-export default connect(mapStateToProps, { getModelList, getWSSPrimitiveFields })(ViewStream);
+export default connect(mapStateToProps, { getModelList, getAnEntityOfModelList, getWSSPrimitiveFields })(ViewStream);
