4171d8e36c1db151ceec8cfa266fff6ca36caf63
haniyemli <mollayi@dmlcc.com>
2023-12-25 15:28:40 +0330

    timer part Footer.js

--- frontend/src/app/Footer.js
+++ frontend/src/app/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-white text-center text-gray-700">
       <Timer />
-      <div className="space-y-6 ps-28 pe-28">
+      <div className="space-y-6 pe-28 ps-28">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-4">
             <a
@@ -142,13 +142,28 @@ export default function Footer() {
             </div>
           </a>
         </div>
-        <div className="flex items-center justify-between text-gray-400 pb-4">
+        <div className="flex items-center justify-between pb-4 text-gray-400">
           <div className="flex items-center gap-4">
-            <p className="mt-6 text-sm">About us</p>
-            <p className="mt-6 text-sm">Contact</p>
-            <p className="mt-6 text-sm">Privacy Policy</p>
-            <p className="mt-6 text-sm">Sitemap</p>
-            <p className="mt-6 text-sm">Terms of Use</p>
+            <a
+                href=""
+                className="flex items-center justify-center gap-2"
+            ><p className="mt-6 text-sm">About us</p></a>
+            <a
+                href=""
+                className="flex items-center justify-center gap-2"
+            ><p className="mt-6 text-sm">Contact</p></a>
+              <a
+                  href=""
+                  className="flex items-center justify-center gap-2"
+              ><p className="mt-6 text-sm">Privacy Policy</p></a>
+                <a
+                    href=""
+                    className="flex items-center justify-center gap-2"
+                ><p className="mt-6 text-sm">Sitemap</p></a>
+                  <a
+                      href=""
+                      className="flex items-center justify-center gap-2"
+                  ><p className="mt-6 text-sm">Terms of Use</p></a>
           </div>
           <div className="flex items-center justify-between">
             <p className="mt-6 text-sm">© 2000-2021, All Rights Reserved</p>
@@ -159,18 +174,74 @@ export default function Footer() {
   );
 }
 
-function Timer(){
-  return(
-      <div
-          className="relative bg-cover bg-no-repeat bg-center h-64 flex items-center justify-center"
-          style={{ backgroundImage: 'url(/source/footer_rectangle.svg)' ,
-            backgroundSize: '100% auto', // Adjust this line to control background size
-          }}
-      >
-        <div className="absolute text-white text-center">
-          <h1 className="text-4xl font-bold mb-4">Your Text Here</h1>
-          <p className="text-lg">More Text Goes Here</p>
+function Timer() {
+  return (
+    <div
+      className="relative flex h-64 items-center justify-between bg-cover bg-center text-white bg-no-repeat px-28 py-5"
+      style={{
+        backgroundImage: 'url(/source/footer_rectangle.svg)',
+        backgroundSize: '100% auto', // Adjust this line to control background size
+      }}
+    >
+        <div className="max-w-screen-lg justify-start items-start">
+          <p className="text-xs uppercase text-left">Overline Goes Here</p>
+          <h1 className="text-4xl font-semibold py-2 text-left">Sign Up and Enjoy it</h1>
         </div>
-      </div>
-  )
+        <div className="flex space-x-4">
+          <div className="flex flex-col items-center">
+            <span className="text-2xl font-normal border border-opacity-25 border-white rounded-md p-2 ">
+              21
+            </span>
+            <span className="text-xs pt-3">DAYS</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-3xl font-semibold py-2">:</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-2xl font-normal border border-opacity-25 border-white rounded-md p-2 ">
+              16
+            </span>
+            <span className="text-xs pt-3">HOURS</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-3xl font-semibold py-2">:</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-2xl font-normal border border-opacity-25 border-white rounded-md p-2 ">
+              05
+            </span>
+            <span className="text-xs pt-3">MINUTES</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-3xl font-semibold py-2">:</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-2xl font-normal border border-opacity-25 border-white rounded-md p-2 ">
+              45
+            </span>
+            <span className="text-xs pt-3">SECONDS</span>
+          </div>
+
+        </div>
+      <button className="bg-white text-purple-950 py-3 px-6 rounded-md hover:bg-gray-100 transition-colors font-semibold">
+        Register Now →
+      </button>
+
+
+      {/*<div className="flex items-center gap-4 text-white">*/}
+      {/*  <div className="mx-auto max-w-screen-lg space-y-1">*/}
+      {/*    <p className="flex items-center justify-start text-xs">Overline Goes Here</p>*/}
+      {/*    <p className="flex items-center justify-start text-3xl font-semibold">Sign Up and Enjoy it</p>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+      {/*<div className="flex items-center gap-4 text-white">*/}
+      {/*  <h1 className="mb-4 text-4xl font-bold">Your Text Here</h1>*/}
+      {/*  <p className="text-lg">More Text Goes Here</p>*/}
+      {/*</div>*/}
+      {/*<div className="flex items-center gap-4 text-white">*/}
+      {/*  <h1 className="mb-4 text-4xl font-bold">Your Text Here</h1>*/}
+      {/*  <p className="text-lg">More Text Goes Here</p>*/}
+      {/*</div>*/}
+    </div>
+  );
 }
