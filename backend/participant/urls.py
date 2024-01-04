0c61c2f834b1caf59d76b79ac9713937c0b3f349
Mohammadmahdi <fmohammadmahdi1381@gmail.com>
2024-01-04 19:26:56 +0330

    password_reset

--- backend/participant/urls.py
+++ backend/participant/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from participant import views
 
 urlpatterns = [
-    path('sign-up/', views.ParticipantCreateAPIView.as_view(), name='Sign Up'),
+    path('sign-up/', views.ParticipantCreateAPIView.as_view(), name='sign-up'),
+    path('password-reset/', views.PasswordResetAPIView.as_view(), name='password-reset'),
 ]
--- backend/participant/views.py
+++ backend/participant/views.py
@@ -1,4 +1,10 @@
-from rest_framework import permissions, generics
+from random import choices
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.http import Http404
+from rest_framework import permissions, generics, views, status
+from rest_framework.response import Response
 
 from participant.serializers import ParticipantSerializer
 from participant.models import Participant
@@ -8,3 +14,46 @@ class ParticipantCreateAPIView(generics.CreateAPIView):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
     permission_classes = [permissions.AllowAny, ]
+
+
+class PasswordResetAPIView(views.APIView):
+    permission_classes = [permissions.AllowAny, ]
+
+    def post(self, request):
+        participant = Participant.objects.filter(
+            user__email=request.data['email']).last()
+        if not participant:
+            raise Http404
+        participant.password_reset_code = ''.join(
+            choices([str(i) for i in range(10)], k=17))
+        participant.save()
+        send_mail(
+            'WSS Password Reset',
+            f'Your WSS password reset link is: https://wss.ce.sharif.edu/password-reset/{participant.password_reset_code}',
+            settings.EMAIL_HOST_USER,
+            [participant.user.email],
+        )
+        return Response(
+            'Password reset code sent to the email address ({0})'.format(
+                participant.user.email),
+            status=status.HTTP_200_OK
+        )
+
+    def put(self, request):
+        try:
+            participant = Participant.objects.get(
+                user__email=request.data['email'])
+        except Participant.DoesNotExist:
+            raise Http404
+        if participant.password_reset_code == request.data['token']:
+            participant.user.set_password(request.data['password'])
+            participant.user.save()
+            return Response(
+                'Password reset successfully',
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                'Password reset code is not correct',
+                status=status.HTTP_406_NOT_ACCEPTABLE
+            )
