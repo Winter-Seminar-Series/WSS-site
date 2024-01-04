42cf420c46908990b05454a86acbc4c01d1d165d
Hosein Khansari <hosein@gamertag.app>
2024-01-05 00:44:58 +0330

    Bring the landing page back

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,22 +3,15 @@ import Programs from './Programs';
 import Seminar from './seminar/Seminar';
 import Staff from './staff/Staff';
 import Footer from './Footer';
-import Form from './forms/Form';
-import SignUpForm from './forms/SignUpForm';
-import LogInForm from './forms/LogInForm';
-import PasswordResetForm from './forms/PasswordResetForm';
 
 export default function Home() {
   return (
-    <div>
-      {/*<Header />*/}
-      {/*<Programs />*/}
-      {/*<Seminar />*/}
-      {/*<Staff />*/}
-      {/*<Footer />*/}
-      <SignUpForm />
-      <LogInForm />
-      <PasswordResetForm />
-    </div>
+    <>
+      <Header />
+      <Programs />
+      <Seminar />
+      <Staff />
+      <Footer />
+    </>
   );
 }
