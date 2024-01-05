29a6af5642deb0da4a49ae2ada88f295b80f4daa
Hosein Khansari <hoskhansari@gmail.com>
2024-01-05 13:04:15 +0330

    Make the landing page more responsive

--- frontend/src/app/Header.js
+++ frontend/src/app/Header.js
@@ -2,12 +2,12 @@ import Rectangle from '../../public/source/Rectangle.png';
 
 export default function Header() {
   return (
-    <header className="max-lg:w-90 mb-60">
+    <header>
       <div
-        className="max-lg:bg-mobile lg:bg-desktop items-center bg-cover bg-center bg-no-repeat max-lg:h-[896px] max-lg:w-[424px] max-lg:pt-8 lg:h-[900px] lg:pt-8"
+        className="items-center bg-cover bg-center bg-no-repeat pb-56 pt-8"
         style={{ backgroundImage: `url(${Rectangle.src})` }}
       >
-        <div className="mx-auto flex items-center justify-between max-lg:h-[38px] max-lg:w-[312px] lg:mb-8 lg:h-7 lg:w-[1201px]">
+        <nav className="mx-auto flex items-center justify-between px-6 max-lg:h-[38px] lg:mb-8 lg:h-7 lg:max-w-[1200px]">
           <a
             href="home.html"
             className="shrink-0 max-lg:h-[31.8px] max-lg:w-[40.8px] lg:h-[45.129px] lg:w-[57.98px]"
@@ -89,13 +89,13 @@ export default function Header() {
               </p>
             </a>
           </div>
-        </div>
-        <div className="mx-auto flex max-lg:mt-10 max-lg:w-[312px] max-lg:flex-col lg:mt-12 lg:max-w-[1200px] lg:flex-row">
+        </nav>
+        <div className="mx-auto flex px-6 max-lg:mt-10 max-lg:flex-col lg:mt-12 lg:max-w-[1200px] lg:flex-row">
           <div>
-            <p className="max-lg:leading-15 max-lg:text-6xl font-bold not-italic text-white max-lg:pb-6 max-lg:tracking-[-1.2px] lg:pb-10 lg:text-[108px] lg:leading-[106px] lg:tracking-[-2.16px]">
+            <p className="max-lg:leading-15 font-bold not-italic text-white max-lg:pb-6 max-lg:text-6xl max-lg:tracking-[-1.2px] lg:pb-10 lg:text-[108px] lg:leading-[106px] lg:tracking-[-2.16px]">
               WSS - Winter Seminar Series
             </p>
-            <div className="lg:text-2xl font-semibold not-italic leading-[normal] text-white max-lg:text-[21px] max-lg:leading-[30px] max-lg:tracking-[-0.42px] lg:tracking-[-0.48px]">
+            <div className="font-semibold not-italic leading-[normal] text-white max-lg:text-[21px] max-lg:leading-[30px] max-lg:tracking-[-0.42px] lg:text-2xl lg:tracking-[-0.48px]">
               <p className="">
                 Advanced Topics in Computer Science and Engineering
               </p>
@@ -120,7 +120,7 @@ export default function Header() {
                 </div>
               </div>
             </div>
-            <div className="lg:text-5xl flex flex-row items-start font-bold not-italic leading-[normal] text-white max-lg:mb-9 max-lg:items-start max-lg:justify-between max-lg:pt-7 lg:gap-[52px] lg:pt-[52px] lg:tracking-[-0.96px]">
+            <div className="flex flex-row items-start font-bold not-italic leading-[normal] text-white max-lg:mb-9 max-lg:items-start max-lg:justify-between max-lg:pt-7 lg:gap-[52px] lg:pt-[52px] lg:text-5xl lg:tracking-[-0.96px]">
               <div className="flex flex-col items-center justify-center text-white max-lg:gap-1 lg:gap-3">
                 <div className="flex flex-col items-center justify-center rounded-lg border border-solid border-[rgba(255,255,255,0.30)] px-4 pb-3 pt-2 max-lg:text-xl">
                   21
@@ -129,7 +129,7 @@ export default function Header() {
                   DAYS
                 </p>
               </div>
-              <div className="lg:text-5xl font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:tracking-[-0.96px]">
+              <div className="font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:text-5xl lg:tracking-[-0.96px]">
                 :
               </div>
               <div className="flex flex-col items-center justify-center text-white max-lg:gap-1 lg:gap-3">
@@ -140,7 +140,7 @@ export default function Header() {
                   HOURS
                 </p>
               </div>
-              <div className="lg:text-5xl font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:tracking-[-0.96px]">
+              <div className="font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:text-5xl lg:tracking-[-0.96px]">
                 :
               </div>
               <div className="flex flex-col items-center justify-center text-white max-lg:gap-1 lg:gap-3">
@@ -151,7 +151,7 @@ export default function Header() {
                   MINUTES
                 </p>
               </div>
-              <div className="lg:text-5xl font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:tracking-[-0.96px]">
+              <div className="font-bold not-italic leading-[normal] text-white max-lg:text-[32px] max-lg:leading-[normal] max-lg:tracking-[-0.64px] lg:text-5xl lg:tracking-[-0.96px]">
                 :
               </div>
               <div className="flex flex-col items-center justify-center text-white max-lg:gap-1 lg:gap-3">
@@ -164,101 +164,95 @@ export default function Header() {
               </div>
             </div>
           </div>
-          <div className="max-lg:h-37 max-lg:flex-wrap:wrap inline-flex shrink-0 flex-col justify-between max-lg:h-20 max-lg:flex-row max-lg:items-start lg:h-[535px] lg:w-[203px] lg:items-end">
-            <div className="inline-flex shrink-0 flex-col max-lg:gap-6 lg:h-[245px] lg:justify-between">
-              <div className="max-lg:h-12.5 flex flex-col items-end justify-center max-lg:items-start lg:gap-[-4px]">
-                <p className="lg:text-6xl font-bold not-italic leading-[normal] text-white max-lg:text-[40px] max-lg:tracking-[-1.6px] lg:tracking-[-2.4px]">
-                  30+
-                </p>
-                <p className="text-center text-base font-normal not-italic leading-[normal] text-[rgba(255,255,255,0.80)] max-lg:text-sm">
-                  Top Level Masters
-                </p>
-              </div>
-              <div className="max-lg:h-12.5 flex flex-col items-end justify-center max-lg:items-start lg:gap-[-4px]">
-                <p className="lg:text-6xl font-bold not-italic leading-[normal] text-white max-lg:text-[40px] max-lg:tracking-[-1.6px] lg:tracking-[-2.4px]">
-                  15000+
-                </p>
-                <p className="text-center text-base font-normal not-italic leading-[normal] text-[rgba(255,255,255,0.80)] max-lg:text-sm">
-                  Total Subscribers
-                </p>
-              </div>
+          <div className="flex shrink-0 flex-col justify-between gap-y-6 text-end max-lg:flex-row max-lg:flex-wrap max-lg:text-start">
+            <div className="shrink-0 pr-2 max-lg:w-1/2">
+              <p className="text-6xl font-bold leading-snug tracking-tight text-white max-lg:text-[40px]">
+                30+
+              </p>
+              <p className="text-white text-opacity-80 max-lg:text-sm">
+                Top Level Masters
+              </p>
             </div>
-            <div className="inline-flex shrink-0 flex-col max-lg:gap-6 lg:h-[245px] lg:justify-between">
-              <div className="max-lg:h-12.5 flex flex-col items-end justify-center max-lg:items-start lg:gap-[-4px]">
-                <p className="lg:text-6xl font-bold not-italic leading-[normal] text-white max-lg:text-[40px] max-lg:tracking-[-1.6px] lg:tracking-[-2.4px]">
-                  12+
-                </p>
-                <p className="text-center text-base font-normal not-italic leading-[normal] text-[rgba(255,255,255,0.80)] max-lg:text-sm">
-                  High-Quality Seminars
-                </p>
-              </div>
-              <div className="max-lg:h-12.5 flex flex-col items-end justify-center max-lg:items-start lg:gap-[-4px]">
-                <p className="lg:text-6xl font-bold not-italic leading-[normal] text-white max-lg:text-[40px] max-lg:tracking-[-1.6px] lg:tracking-[-2.4px]">
-                  30+
-                </p>
-                <p className="text-center text-base font-normal not-italic leading-[normal] text-[rgba(255,255,255,0.80)] max-lg:text-sm">
-                  Top Level Masters
-                </p>
-              </div>
+            <div className="shrink-0 max-lg:w-1/2">
+              <p className="text-6xl font-bold leading-snug tracking-tight text-white max-lg:text-[40px]">
+                15000+
+              </p>
+              <p className="text-white text-opacity-80 max-lg:text-sm">
+                Total Subscribers
+              </p>
+            </div>
+            <div className="shrink-0 pr-2 max-lg:w-1/2">
+              <p className="text-6xl font-bold leading-snug tracking-tight text-white max-lg:text-[40px]">
+                12+
+              </p>
+              <p className="text-white text-opacity-80 max-lg:text-sm">
+                High-Quality Seminars
+              </p>
+            </div>
+            <div className="shrink-0 max-lg:w-1/2">
+              <p className="text-6xl font-bold leading-snug tracking-tight text-white max-lg:text-[40px]">
+                30+
+              </p>
+              <p className="text-white text-opacity-80 max-lg:text-sm">
+                Top Level Masters
+              </p>
             </div>
           </div>
         </div>
-        <div className="flex flex-row items-center justify-center">
-          <div className="mt-[92px] flex w-4/5 flex-col items-center justify-center rounded-2xl bg-white pb-10 shadow-[0px_30px_60px_0px_rgba(189,192,199,0.10)] lg:flex lg:gap-8">
-            <div className="text-xl font-medium uppercase not-italic leading-[normal] tracking-[0.8px] text-[#1F2B3D] lg:mb-8 lg:mt-10">
-              Presentations from 15+ top level companies and masters
-            </div>
-            <div className="grid grid-cols-8 items-center justify-center gap-x-16 gap-y-4 self-stretch px-16">
-              <a href="https://www.apple.com">
-                <img src="/source/logos/Apple.png" />
-              </a>
-              <a href="https://www.ed.ac.uk">
-                <img src="/source/logos/Edinburgh.png" />
-              </a>
-              <a href="https://www.epfl.ch/en/">
-                <img src="/source/logos/EPFL.png" />
-              </a>
-              <a href="https://www.uga.edu">
-                <img src="/source/logos/Georgia.png" />
-              </a>
-              <a href="https://www.google.com">
-                <img src="/source/logos/Google.png" />
-              </a>
-              <a href="https://www.london.ac.uk">
-                <img src="/source/logos/London.png" />
-              </a>
-              <a href="https://www.sharif.edu">
-                <img src="/source/logos/Sharif.png" />
-              </a>
-              <a href="https://umd.edu">
-                <img src="/source/logos/Maryland.png" />
-              </a>
-              <a href="https://www.microsoft.com/de-de/">
-                <img src="/source/logos/Microsoft.png" />
-              </a>
-              <a href="https://www.mit.edu">
-                <img src="/source/logos/MIT.png" />
-              </a>
-              <a href="https://openai.com">
-                <img src="/source/logos/OpenAI.png" />
-              </a>
-              <a href="https://www.princeton.edu">
-                <img src="/source/logos/Princeton.png" />
-              </a>
-              <a href="https://www.stanford.edu">
-                <img src="/source/logos/Stanford.png" />
-              </a>
-              <a href="https://www.upenn.edu">
-                <img src="/source/logos/UPenn.png" />
-              </a>
-              <a href="https://uwaterloo.ca">
-                <img src="/source/logos/Waterloo.png" />
-              </a>
-              <a href="https://www.universityofcalifornia.edu">
-                <img src="/source/logos/California.png" />
-              </a>
-            </div>
-          </div>
+      </div>
+      <div className="mx-auto -mt-36 mb-24 flex w-4/5 flex-col items-center justify-center gap-4 rounded-2xl bg-white px-[52px] py-10 text-center shadow-[0px_30px_60px_0px_rgba(189,192,199,0.10)] md:gap-8">
+        <div className="text-xl font-medium uppercase not-italic leading-normal tracking-[0.8px] text-[#1F2B3D]">
+          Presentations from 15+ top level companies and masters
+        </div>
+        <div className="grid grid-cols-4 items-center justify-center gap-8 self-stretch md:grid-cols-8 md:gap-x-16 md:gap-y-4">
+          <a href="https://www.apple.com">
+            <img src="/source/logos/Apple.png" />
+          </a>
+          <a href="https://www.ed.ac.uk">
+            <img src="/source/logos/Edinburgh.png" />
+          </a>
+          <a href="https://www.epfl.ch/en/">
+            <img src="/source/logos/EPFL.png" />
+          </a>
+          <a href="https://www.uga.edu">
+            <img src="/source/logos/Georgia.png" />
+          </a>
+          <a href="https://www.google.com">
+            <img src="/source/logos/Google.png" />
+          </a>
+          <a href="https://www.london.ac.uk">
+            <img src="/source/logos/London.png" />
+          </a>
+          <a href="https://www.sharif.edu">
+            <img src="/source/logos/Sharif.png" />
+          </a>
+          <a href="https://umd.edu">
+            <img src="/source/logos/Maryland.png" />
+          </a>
+          <a href="https://www.microsoft.com/de-de/">
+            <img src="/source/logos/Microsoft.png" />
+          </a>
+          <a href="https://www.mit.edu">
+            <img src="/source/logos/MIT.png" />
+          </a>
+          <a href="https://openai.com">
+            <img src="/source/logos/OpenAI.png" />
+          </a>
+          <a href="https://www.princeton.edu">
+            <img src="/source/logos/Princeton.png" />
+          </a>
+          <a href="https://www.stanford.edu">
+            <img src="/source/logos/Stanford.png" />
+          </a>
+          <a href="https://www.upenn.edu">
+            <img src="/source/logos/UPenn.png" />
+          </a>
+          <a href="https://uwaterloo.ca">
+            <img src="/source/logos/Waterloo.png" />
+          </a>
+          <a href="https://www.universityofcalifornia.edu">
+            <img src="/source/logos/California.png" />
+          </a>
         </div>
       </div>
     </header>
--- frontend/src/app/Programs.js
+++ frontend/src/app/Programs.js
@@ -2,18 +2,18 @@ import Image from 'next/image';
 
 export default function Programs() {
   return (
-    <div className="Programs relative h-[1469px] w-full overflow-auto overflow-hidden bg-white">
+    <div className="relative mx-auto overflow-hidden px-6">
       <div className="Ellipse absolute right-[-200px] top-[636px] h-72 w-72 rounded-full bg-zinc-400 bg-opacity-30 blur-3xl" />
       <div className="Ellipse absolute left-[-200px] top-[205px] h-64 w-64 rounded-full bg-pink-300 bg-opacity-30 blur-3xl" />
 
-      <ProgramsArea />
-      <ItemsTag />
-      <Button text={buttonText} />
+      <div className="mx-auto max-w-[993px]">
+        <ProgramsArea />
+        <ItemsTag />
+        <Button text="More About WSS" />
+      </div>
     </div>
   );
 }
-
-const buttonText = 'More About WSS';
 
 const items = [
   {
@@ -41,46 +41,41 @@ const items = [
 
 function ProgramsArea() {
   return (
-    <div className="AutoLayoutVertical absolute left-[calc(50%_-_145px)] top-[0px] inline-flex h-[104px] w-[290px] flex-col items-center justify-start">
-      <div className="OverlineGoesHere font-['Manrope'] text-lg font-medium uppercase tracking-wide text-neutral-400">
+    <div className="mx-auto mb-20 text-center">
+      <div className="text-lg font-medium uppercase tracking-wide text-neutral-400">
         Overline Goes Here
       </div>
-      <div className="Programs font-['Manrope'] text-[64px] font-bold text-slate-800">
-        Programs
-      </div>
+      <div className="text-[64px] font-bold text-slate-800">Programs</div>
     </div>
   );
 }
 
 function Button({ text }) {
   return (
-    <div className="AutoLayoutHorizontal absolute left-[calc(50%_-_121px)] top-[1298px] mt-10 flex h-16 w-64 items-center items-center justify-center gap-2.5 rounded-md bg-secondary px-8 text-white">
-      <div className="font-['Manrope'] text-lg font-bold">{text}</div>
-      <div className="relative h-6 w-6">
+    <div className="mb-32 mt-20 flex justify-center">
+      <button className="inline-flex h-16 items-center justify-center gap-2.5 gap-x-2.5 rounded-md bg-secondary px-8 text-white">
+        <span className="text-lg font-bold">{text}</span>
         <Image
           src={'/source/arrow_right_white.svg'}
           alt=""
           width={20}
           height={20}
-          className={'ml-3'}
         />
-      </div>
+      </button>
     </div>
   );
 }
 
 function ItemText({ title, overline, description }) {
   return (
-    <div className="AutoLayoutVertical inline-flex h-[100%] w-96 flex-col items-start justify-start gap-2.5">
-      <div className="AutoLayoutVertical flex flex-col items-start justify-start">
-        <div className="font-['Manrope'] text-[16px] font-medium uppercase tracking-wide text-neutral-400">
-          {overline}
-        </div>
-        <div className="title font-['Manrope'] text-[48px] font-bold text-slate-800">
-          {title}
-        </div>
+    <div className="grow">
+      <div className="text-base font-medium uppercase tracking-wide text-neutral-400">
+        {overline}
       </div>
-      <div className="description w-96 font-['Manrope'] text-[18px] font-normal leading-relaxed text-neutral-400">
+      <div className="text-5xl font-bold leading-normal text-slate-800">
+        {title}
+      </div>
+      <div className="mt-2.5 text-lg leading-relaxed text-neutral-400">
         {description}
       </div>
     </div>
@@ -88,50 +83,35 @@ function ItemText({ title, overline, description }) {
 }
 
 function ItemImg({ img }) {
-  if (img.localeCompare('') == 0)
-    return (
-      <div className="Image h-72 shrink grow basis-0 rounded-lg bg-gray-200" />
-    );
-  else
-    return (
-      <div className="Image h-[300px] w-[433px] rounded-lg">
-        <img
-          className="relative h-full w-full shrink-0 overflow-hidden object-cover"
-          alt=""
-          src={img}
-        />
-      </div>
-    );
+  return (
+    <img
+      className="relative aspect-video w-full max-w-sm shrink-0 rounded-lg object-cover"
+      alt=""
+      src={img}
+    />
+  );
 }
 
 function ItemTag({ item }) {
-  if (item.id % 2 == 1)
-    return (
-      <div className="AutoLayoutHorizontal flex h-[300px] w-[993px] items-center items-center justify-start gap-20">
-        <ItemText
-          title={item.title}
-          overline={item.overline}
-          description={item.desc}
-        />
-        <ItemImg img={item.img} />
-      </div>
-    );
-  else
-    return (
-      <div className="AutoLayoutHorizontal inline-flex h-[300px] w-[993px] items-center justify-start gap-20">
-        <ItemImg img={item.img} />
-        <ItemText
-          title={item.title}
-          overline={item.overline}
-          description={item.desc}
-        />
-      </div>
-    );
+  return (
+    <div
+      className={`flex flex-col items-center justify-start gap-x-20 gap-y-8 ${
+        item.id % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'
+      }`}
+    >
+      <ItemImg img={item.img} />
+      <ItemText
+        title={item.title}
+        overline={item.overline}
+        description={item.desc}
+      />
+    </div>
+  );
 }
 
 function ItemsTag() {
   return (
-    <div className="relative left-[calc(50%_-_497px)] top-[174px] flex w-[993px] flex-col items-end justify-start gap-[72px] text-base">
+    <div className="mx-auto flex flex-col items-end justify-start gap-[72px] text-base">
       {items.map((item) => (
         <ItemTag item={item} key={item.id} />
       ))}
