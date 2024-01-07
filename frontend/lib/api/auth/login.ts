4d8cec4f583ecef2fe98af52a852279020562a26
Soroush Sherafat <sorousherafat@gmail.com>
2024-01-07 21:04:01 +0330

    added login refresh base functionality

--- frontend/lib/api/auth/login.ts
+++ frontend/lib/api/auth/login.ts
@@ -27,6 +27,17 @@ async function callLoginAPI(email: string, password: string) {
   });
 }
 
+async function callRefreshAPI(refresh: string) {
+  const url = 'http://127.0.0.1:8000/api/sign-in/refresh/';
+
+  const formData = formDataFromObject({ refresh });
+
+  return await fetchJson<LoginResponse>(url, {
+    method: 'POST',
+    body: formData,
+  });
+}
+
 async function saveLoginToSession(data: LoginResponse) {
   const session = await getSession();
   session.isLoggedIn = true;
@@ -45,4 +56,16 @@ export default async function login(formData: FormData) {
   await saveLoginToSession(data);
 
   redirect('/dashboard/profile');
+}
+
+export async function refresh() {
+  const session = await getSession();
+
+  if (!session.refreshToken) {
+    throw new Error('Not already signed in.');
+  }
+
+  const data = await callRefreshAPI(session.refreshToken);
+
+  await saveLoginToSession(data);
 }
