684bf7c0cadd646a0d02788aa5288648454403ad
Mohammadmahdi <fmohammadmahdi1381@gmail.com>
2024-01-08 15:25:30 +0330

    added admin-panel for core models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from core.models import *
+
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(SubEvent)
+admin.site.register(Speaker)
+admin.site.register(Seminar)
+admin.site.register(Workshop)
+admin.site.register(RoundTable)
+admin.site.register(LabTalk)
+admin.site.register(PosterSession)
