49a24effc206db69b68fd316d761ca3e10744d61
Soroush Sherafat <sorousherafat@gmail.com>
2024-01-09 22:02:40 +0330

    added profile get API to frontend

---
+++ frontend/src/lib/api/fetch.ts
@@ -0,0 +1,104 @@
+import camelcaseKeys from 'camelcase-keys';
+import decamelizeKeys from 'decamelize-keys';
+import { getSession } from './session';
+import { isAuthenticated } from '../auth';
+import { redirect } from 'next/navigation';
+import { refresh } from './auth/login';
+
+export class FetchError extends Error {
+  response: Response;
+  data: {
+    status: number;
+    message: string;
+  };
+  constructor({
+    status,
+    message,
+    response,
+    data,
+  }: {
+    status: number;
+    message: string;
+    response: Response;
+    data: {
+      status: number;
+      message: string;
+    };
+  }) {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, FetchError);
+    }
+
+    this.name = 'FetchError';
+    this.response = response;
+    this.data = data ?? { message, status };
+  }
+}
+
+export async function fetchJson<JSON = unknown>(
+  input: RequestInfo,
+  body?: object,
+  init?: Omit<RequestInit, 'body'>,
+): Promise<JSON> {
+  const fetchInit: RequestInit = init ?? {};
+
+  fetchInit.headers = {
+    ...init?.headers,
+    'Content-Type': 'application/json',
+  };
+
+  if (body) {
+    fetchInit.body = JSON.stringify(decamelizeKeys(body, { deep: true }));
+  }
+
+  console.log(fetchInit.headers);
+
+  const response = await fetch(input, fetchInit);
+
+  const snakeCaseData = await response.json();
+  const camelCaseData = camelcaseKeys(snakeCaseData, { deep: true });
+
+  if (response.ok) {
+    return camelCaseData;
+  }
+
+  throw new FetchError({
+    status: response.status,
+    message: response.statusText,
+    response,
+    data: camelCaseData,
+  });
+}
+
+export async function fetchJsonWithAuth<JSON = unknown>(
+  input: RequestInfo,
+  body?: object,
+  init?: Omit<RequestInit, 'body'>,
+) {
+  if (!(await isAuthenticated())) {
+    redirect('/login');
+  }
+
+  const session = await getSession();
+  const accessToken = session.accessToken;
+
+  init ??= {};
+
+  init.headers = {
+    ...init?.headers,
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  try {
+    return await fetchJson<JSON>(input, body, init);
+  } catch (error) {
+    if (error instanceof FetchError && error.data.status === 401) {
+      await refresh();
+      return await fetchJson<JSON>(input, body, init);
+    } else {
+      throw error;
+    }
+  }
+}
