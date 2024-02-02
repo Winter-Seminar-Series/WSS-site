1c07fef2525b67a0fbab0058244c8a452e63385a
AlisaLC <ali_sal1381@yahoo.com>
2024-02-02 23:36:07 +0330

    added log

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -13,6 +13,11 @@ from payment.serializers import PaymentRequestCreateSerializer, PaymentRequestPr
 from payment.models import PaymentRequest
 from participant.models import Participant, ParticipantInfo, Participation
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 class PaymentRequestCreateAPIView(generics.CreateAPIView):
     serializer_class = PaymentRequestCreateSerializer
@@ -45,34 +50,46 @@ class PaymentRequestVerifyAPIView(generics.GenericAPIView):
     
     def get(self, request, *args, **kwargs):
         if 'pk' not in self.kwargs:
+            logger.error('Payment request not found')
             raise serializers.ValidationError('Payment request not found')
         pk = self.kwargs['pk']
         try:
             participant = Participant.objects.get(user=self.request.user)
+            logger.info(f'Participant {participant.pk} is verifying payment request {pk}')
             instance = PaymentRequest.objects.get(pk=pk)
+            logger.info(f'Payment request {pk} found')
         except:
+            logger.error('Participant or request not found')
             raise serializers.ValidationError('Participant or request not found')
         if instance.participant != participant:
+            logger.error('Participant does not match')
             raise serializers.ValidationError('Participant does not match')
         if instance.paid:
+            logger.error('Payment request already paid')
             return instance
         url = f'{settings.PAYMENT_SERVICE_URL}/transaction?order_id={instance.order_id}'
         res = requests.get(url)
+        logger.info(f'Payment service response: {res.status_code} {res.text}')
         if res.status_code != 200:
             raise serializers.ValidationError('Payment service error')
         res = res.json()
         if res['payment_status'] == 'success':
+            logger.info('Payment request paid')
             instance.paid = True
             info = instance.participant.info
             info.pk = None
             info.save()
+            logger.info(f'Participant info {info.pk} created')
             for plan in instance.plans.all():
                 Participation.objects.create(
                     participant=instance.participant,
                     plans=plan,
                     info=info
                 )
+                logger.info(f'Participation for plan {plan.pk} created')
         else:
+            logger.error('Payment request not paid')
             instance.paid = False
         instance.save()
+        logger.info(f'Payment request {pk} updated')
         return Response(instance)
