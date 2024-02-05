57dba12f45907400ec2f1039135781312d1a59fb
Soroush Sherafat <sorousherafat@gmail.com>
2024-02-05 18:31:44 +0330

    fixed bugs in workshop list page

--- frontend/src/app/workshop/WorkshopCard.tsx
+++ frontend/src/app/workshop/WorkshopCard.tsx
@@ -3,7 +3,6 @@ import calender from './assets/Calendar.svg';
 import category from './assets/Category.svg';
 import Image from 'next/image';
 import { Workshop } from '../../lib/types';
-// import SeminarLogo from './SeminarLogo';
 
 export default function WorkshopCard({ workshop }: { workshop: Workshop }) {
   const formatDate = (date: Date) => {
@@ -11,7 +10,7 @@ export default function WorkshopCard({ workshop }: { workshop: Workshop }) {
       month: 'long', // 'numeric', '2-digit', 'long', 'short', or 'narrow'
       day: 'numeric', // 'numeric' or '2-digit'
     };
-    return date.toLocaleDateString('en-US', options);
+    return date?.toLocaleDateString('en-US', options);
   };
 
   return (
@@ -21,12 +20,10 @@ export default function WorkshopCard({ workshop }: { workshop: Workshop }) {
       }
     >
       <div className="relative z-10 mx-auto h-[231px] w-[347px] overflow-hidden rounded-lg bg-cover">
-        <Image
-          src={workshop.thumbnail}
-          alt={workshop.name}
-          layout="fill"
-          objectFit="cover"
-          className="z-0"
+        <img
+          src={workshop?.thumbnail ?? '/source/dashboard/register/sample.svg'}
+          alt={workshop?.name}
+          className="z-0 w-full"
         />
       </div>
 
@@ -40,21 +37,21 @@ export default function WorkshopCard({ workshop }: { workshop: Workshop }) {
           <div className="flex items-center space-x-2">
             <Image src={calender} alt="Calendar" width={20} height={20} />
             <span>
-              {formatDate(workshop.startDate).toUpperCase()} -{' '}
-              {formatDate(workshop.endDate).toUpperCase()}{' '}
+              {formatDate(workshop?.startDate)?.toUpperCase()} -{' '}
+              {formatDate(workshop?.endDate)?.toUpperCase()}{' '}
             </span>
           </div>
           <div className="flex items-center space-x-1">
             <Image src={category} alt="Category" width={20} height={20} />
-            <span>{workshop.sessions.length} MODULES</span>
+            <span>{workshop?.sessions?.length ?? 0} MODULES</span>
           </div>
         </div>
         <div className="verflow-hidden line-clamp-2 w-full overflow-ellipsis pb-1 pt-3.5 text-xl font-semibold text-black">
-          {workshop.name}
+          {workshop?.name}
         </div>
         <div className="flex items-center space-x-1">
           <div className="pb-1 pt-3.5 text-xl font-semibold text-black">
-            {workshop.price.toLocaleString()}
+            {(workshop?.price ?? 0).toLocaleString()}
           </div>
           <div className="pt-4 text-base font-medium text-black">Rial</div>
         </div>
