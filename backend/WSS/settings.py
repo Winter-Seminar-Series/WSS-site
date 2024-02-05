d0714eeda6ae64812a14e61a14c005b1a8a0334d
AlisaLC <ali_sal1381@yahoo.com>
2024-02-06 02:37:02 +0330

    added static root

--- backend/WSS/settings.py
+++ backend/WSS/settings.py
@@ -185,6 +185,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
