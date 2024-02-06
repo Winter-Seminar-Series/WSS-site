ec9c5b811985d0498e868cf21023803a6cdb9141
AlisaLC <ali_sal1381@yahoo.com>
2024-02-06 15:27:10 +0330

    added upload from csv

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,7 +1,42 @@
+import codecs
+from django import forms
 from django.contrib import admin
+import csv
+from django.urls import path
 
 from payment.models import PaymentDiscount, PaymentRequest
 
 # Register your models here.
-admin.site.register(PaymentDiscount)
+
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
+
+class PaymentDiscountAdmin(admin.ModelAdmin):
+    search_fields = ('discount_code',)
+    list_display = ('discount_code', 'percentage', 'amount', 'count')
+
+    change_list_template = "discount_changelist.html"
+
+    def import_from_csv(self, request, queryset):
+        if request.method == 'POST':
+            csv_file = request.FILES["csv_file"]
+            reader = csv.reader(codecs.iterdecode(csv_file, 'UTF-8'))
+            for row in reader:
+                _, created = PaymentDiscount.objects.get_or_create(
+                    discount_code=row[0],
+                    percentage=row[1],
+                    amount=row[2],
+                    count=row[3]
+                )
+            self.message_user(request, "Your csv file has been imported")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_from_csv),
+        ]
+        return my_urls + urls
+
+admin.site.register(PaymentDiscount, PaymentDiscountAdmin)
+
 admin.site.register(PaymentRequest)
