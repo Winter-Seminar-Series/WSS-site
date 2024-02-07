25b3d89c53259ee458ba9c8cca425cc2ad76a68c
alishahali1382 <alishahali1382@gmail.com>
2024-02-07 21:00:59 +0330

    fix seminars url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('sign-in/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('sign-in/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('event/', EventAPIView.as_view(), name='events'),
-    path('seminar/', SeminarAPIView.as_view(), name='seminars'),
+    path('seminar/<int:event_id>/', SeminarAPIView.as_view(), name='seminars'),
     path('speaker/<pk>/', SpeakerAPIView.as_view(), name='speaker'),
 ]
