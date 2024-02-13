78dc237f7bf3026b9831f5aaefbb4fe1c4b6af50
Soroush Sherafat <sorousherafat@gmail.com>
2024-02-13 23:35:43 +0330

    added time to workshop modules

--- frontend/src/lib/api/events/speaker.ts
+++ frontend/src/lib/api/events/speaker.ts
@@ -1,8 +1,18 @@
+import { convertMarkdownToHTML } from '../../markdown';
 import { Speaker } from '../../types';
 import { fetchJson } from '../fetch';
 
 export async function fetchSpeakerById(id: number) {
   const url = `${process.env.API_ORIGIN}/api/speaker/${id}/`;
 
-  return await fetchJson<Speaker>(url);
+  const speakerResponse = await fetchJson<Speaker>(url);
+
+  const description = await convertMarkdownToHTML(speakerResponse.description);
+
+  const speaker: Speaker = {
+    ...speakerResponse,
+    description,
+  };
+
+  return speaker;
 }
--- frontend/src/ui/components/events/workshops/WorkshopModules.tsx
+++ frontend/src/ui/components/events/workshops/WorkshopModules.tsx
@@ -3,6 +3,27 @@
 import Image from 'next/image';
 import { Disclosure } from '@headlessui/react';
 import { WorkshopSession } from '../../../../lib/types';
+import calender from '../../../../app/workshops/assets/Calendar.svg';
+import clock from '../../../../app/workshops/assets/Clock.svg';
+import { getTimeSpanInMinutes } from '../../../../lib/date';
+
+const formatDate = (date: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'long', // 'numeric', '2-digit', 'long', 'short', or 'narrow'
+    day: 'numeric', // 'numeric' or '2-digit'
+  };
+  return date?.toLocaleDateString('en-US', options);
+};
+
+const formatTime = (time: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+  return time?.toLocaleString('en-US', options);
+};
 
 export default function WorkshopModules({
   sessions,
@@ -26,13 +47,34 @@ export default function WorkshopModules({
                 <span>
                   Module {index + 1}: {session.name} ({session.speaker.name})
                 </span>
-                <Image
-                  src="/Arrow.svg"
-                  alt=""
-                  width={24}
-                  height={24}
-                  className={open ? 'rotate-180' : ''}
-                />
+                <div className="flex items-center space-x-4 text-sm font-medium uppercase not-italic leading-[normal] tracking-[0.72px] text-[#8A8998]">
+                  <div className="flex justify-end">
+                    <div className="flex items-center space-x-2 uppercase">
+                      <Image src={calender} alt="" width={16} height={16} />
+                      <span>
+                        {formatDate(session.date)},{' '}
+                        {formatTime(session.startingTime)}
+                      </span>
+                    </div>
+                    <div className="flex items-center space-x-1 pl-3">
+                      <Image src={clock} alt="" width={16} height={16} />
+                      <span>
+                        {getTimeSpanInMinutes(
+                          session.startingTime,
+                          session.endingTime,
+                        )}{' '}
+                        MINUTES
+                      </span>
+                    </div>
+                  </div>
+                  <Image
+                    src="/Arrow.svg"
+                    alt=""
+                    width={24}
+                    height={24}
+                    className={open ? 'rotate-180' : ''}
+                  />
+                </div>
               </Disclosure.Button>
               <Disclosure.Panel className="p-4">
                 <div
