1c95f74a1a6d295b0138372ee3c2e1d47e8bca64
Soroush Sherafat <sorousherafat@gmail.com>
2024-07-22 14:49:30 +0200

    updated `next.config.js` for use with nginx

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['wss-sharif.com', '127.17.0.1:3000'],
+    },
+  },
 };
 
 module.exports = nextConfig;
