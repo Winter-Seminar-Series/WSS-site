e95f0def33da25a25419bcc0b9a35e51e552a582
Moeein Aali <moeeeinaali@gmail.com>
2025-01-07 22:49:01 +0330

    fix: dropdown full row clickable

--- frontend/src/ui/components/dashboard/DropDown.tsx
+++ frontend/src/ui/components/dashboard/DropDown.tsx
@@ -54,23 +54,28 @@ export default function Dropdown({ transparent }) {
       </button>
 
       {isOpen && (
-        <div className=" absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white opacity-90 shadow-lg ring-1 ring-black ring-opacity-5">
+        <div
+          className=" absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white opacity-90 shadow-lg ring-1 ring-black ring-opacity-5">
           <ul className="py-2">
             {historyData.map((value, index) => {
               return (
-                <li
-                  key={value.url}
-                  className="group flex transform animate-slideIn cursor-pointer items-center px-4 py-2 text-sm text-gray-700 opacity-0"
-                  style={{
-                    animationDelay: `${index * 100}ms`,
-                  }}
-                >
-                  <Logo className="mr-2 h-3 w-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:w-[18px] lg:w-[24px]" />
-                  <a href={value.url} target={'_blank'}>
+                <a href={value.url} target={'_blank'}>
+                  <li
+                    key={value.url}
+                    className="group flex transform animate-slideIn cursor-pointer items-center px-4 py-2 text-sm text-gray-700 opacity-0"
+                    style={{
+                      animationDelay: `${index * 100}ms`,
+                    }}
+                  >
+                    <Logo
+                      className="mr-2 h-3 w-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100 sm:w-[18px] lg:w-[24px]" />
+
                     {value.name}
-                  </a>
-                  <div className="absolute bottom-0 left-[15%] h-0.5 w-[70%] origin-left scale-x-0 bg-black transition-all duration-500 ease-in-out group-hover:scale-x-100"></div>
-                </li>
+
+                    <div
+                      className="absolute bottom-0 left-[15%] h-0.5 w-[70%] origin-left scale-x-0 bg-black transition-all duration-500 ease-in-out group-hover:scale-x-100"></div>
+                  </li>
+                </a>
               );
             })}
           </ul>
