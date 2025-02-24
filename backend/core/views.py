8f64606a15d0b9793c788acec7dd47817b7cef7c
Sadegh Sargeran <sadegh.sg.sn@gmail.com>
2025-02-24 21:12:20 +0330

    fix PosterSessionImage retrieving

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
-from rest_framework.exceptions import NotFound
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from core.models import Event, LabTalk, Seminar, Speaker, RoundTable, PosterSession, PosterSessionImage
 from core.serializers import EventSerializer, SeminarSerializer, SpeakerSerializer, RoundTableSerializer, \
@@ -69,7 +68,34 @@ class PosterSessionImageAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PosterSessionImageSerializer
 
     def get_object(self):
-        return get_object_or_404(PosterSessionImage, user=self.request.user)
+        try:
+            # Try to get the PosterSessionImage object for the current user
+            return PosterSessionImage.objects.get(user=self.request.user)
+        except PosterSessionImage.DoesNotExist:
+            # Return None if the object does not exist
+            return None
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance is None:
+            # Return an empty response with 200 OK status
+            return Response({}, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance is None:
+            # Return an empty response with 404 NOT FOUND status
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance is None:
+            # Return an empty response with 404 NOT FOUND status
+            return Response({}, status=status.HTTP_404_NOT_FOUND)
+        return super().destroy(request, *args, **kwargs)
 
 
 class PosterSessionImageCreateAPIView(generics.CreateAPIView):
