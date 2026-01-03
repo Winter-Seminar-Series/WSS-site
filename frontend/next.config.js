e0ea08348455897daa4d361ef95961072e1d190c
Moeein Aali <moeeeinaali@gmail.com>
2026-01-03 16:09:01 +0330

    [REFACTOR] fix allowedOrigins

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['https://wss-sharif.com', 'http://frontend:3000'],
+    },
+  },
+};
 
 module.exports = nextConfig;
